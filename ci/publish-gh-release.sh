83f57c85e6384e126bc582d0409f2ffa3405de57
Amir Tuval <amirtu@codota.com>
2020-09-24 17:41:10 +0300

    fix artifact name in beta

--- ci/publish-gh-release.sh
+++ ci/publish-gh-release.sh
@@ -7,4 +7,10 @@ cd "$(dirname "$0")/.."
 CURRENT_VERSION=$(./gradlew -q currentVersion)
 CHANNEL=$1
 
-ci/bin/ghr -n "v$CURRENT_VERSION-$CHANNEL" "$CURRENT_VERSION-$CHANNEL" build/distributions/TabNine-$CURRENT_VERSION.zip
+if [ "$CHANNEL" = "ga" ]; then
+  FILE_VERSION=$CURRENT_VERSION
+else
+  FILE_VERSION=$CURRENT_VERSION.$CHANNEL
+fi
+
+ci/bin/ghr -n "v$CURRENT_VERSION-$CHANNEL" "$CURRENT_VERSION-$CHANNEL" build/distributions/TabNine-"$FILE_VERSION".zip
