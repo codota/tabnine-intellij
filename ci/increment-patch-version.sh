341400895ada1b7a68c943cb59df186995eda029
Amir Tuval <amirtu@codota.com>
2020-09-24 16:48:54 +0300

    increment-patch-version script

---
+++ ci/increment-patch-version.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+set -e
+
+VERSION=$1
+
+cd "$(dirname "$0")/.."
+
+CURRENT_VERSION=$(./gradlew -q currentVersion)
+
+increment_version ()
+{
+  declare -a part=( ${1//\./ } )
+  declare    new
+  declare -i carry=1
+
+  for (( CNTR=${#part[@]}-1; CNTR>=0; CNTR-=1 )); do
+    len=${#part[CNTR]}
+    new=$((part[CNTR]+carry))
+    [ ${#new} -gt $len ] && carry=1 || carry=0
+    [ $CNTR -gt 0 ] && part[CNTR]=${new: -len} || part[CNTR]=${new}
+  done
+  new="${part[*]}"
+  echo -e "${new// /.}"
+}
+
+VERSION=$(increment_version $CURRENT_VERSION)
+
+sed -i "s/: '$CURRENT_VERSION'$/: '$VERSION'/g" build.gradle
