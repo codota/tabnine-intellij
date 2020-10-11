5af3ae2aecaec23dcf5b2b8c4c803aac65e5a7c5
Amir Tuval <amirtu@codota.com>
2020-10-11 12:40:38 +0300

    fix publish-gh-release.sh` script to based on file name, so that we know the exact version for beta

--- ci/publish-gh-release.sh
+++ ci/publish-gh-release.sh
@@ -8,9 +8,12 @@ CURRENT_VERSION=$(./gradlew -q currentVersion)
 CHANNEL=$1
 
 if [ "$CHANNEL" = "ga" ]; then
-  FILE_VERSION=$CURRENT_VERSION
+  FILE_NAME=build/distributions/TabNine-$CURRENT_VERSION.zip
 else
-  FILE_VERSION=$CURRENT_VERSION.$CHANNEL
+  FILE_NAME=$(ls -t build/distributions/TabNine-*.zip | grep $CHANNEL | head -1)
 fi
 
-ci/bin/ghr -n "v$CURRENT_VERSION-$CHANNEL" "$CURRENT_VERSION-$CHANNEL" build/distributions/TabNine-"$FILE_VERSION".zip
+STRIPPED_FILE_NAME=$(basename $FILE_NAME .zip)
+RELEASE_NAME=${STRIPPED_FILE_NAME:8}
+
+ci/bin/ghr -n "v$RELEASE_NAME" "$RELEASE_NAME" $FILE_NAME
