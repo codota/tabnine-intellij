991f07763b4914df16405f241eb3625b87e931dc
Amir Tuval <amirtu@codota.com>
2021-02-10 12:11:47 +0200

    in alpha, show the origin in completions instead of the brand name

--- channels/alpha/com/tabnine/config/Config.java
+++ channels/alpha/com/tabnine/config/Config.java
@@ -2,4 +2,5 @@ package com.tabnine.config;
 
 public class Config {
     public final static String CHANNEL = "alpha";
+    public final static Boolean DISPLAY_ORIGIN = true;
 }
--- channels/production/com/tabnine/config/Config.java
+++ channels/production/com/tabnine/config/Config.java
@@ -2,4 +2,5 @@ package com.tabnine.config;
 
 public class Config {
     public final static String CHANNEL = "production";
+    public final static Boolean DISPLAY_ORIGIN = false;
 }
