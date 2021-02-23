d6e645581b303baa077e30985e3148c59cfb15e9
Boaz Berman <boaz@codota.com>
2021-02-23 14:21:10 +0200

    Fix NullPointerException on engine process destroy

---
+++ src/main/java/com/tabnine/binary/BinaryProcessGateway.kt
@@ -0,0 +1,38 @@
+package com.tabnine.binary
+
+import com.tabnine.binary.exceptions.TabNineDeadException
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.nio.charset.StandardCharsets
+
+open class BinaryProcessGateway {
+    private var process: Process? = null
+    private var reader: BufferedReader? = null
+
+    @Throws(IOException::class)
+    open fun init(command: List<String?>?) {
+        val createdProcess = ProcessBuilder(command).start()
+
+        process = createdProcess
+        reader = BufferedReader(InputStreamReader(createdProcess.inputStream, StandardCharsets.UTF_8))
+    }
+
+    @Throws(IOException::class, TabNineDeadException::class)
+    open fun readRawResponse(): String {
+        return reader?.readLine() ?: throw TabNineDeadException("End of stream reached")
+    }
+
+    @Throws(IOException::class)
+    open fun writeRequest(request: String) {
+        process?.outputStream?.write(request.toByteArray(StandardCharsets.UTF_8))
+        process?.outputStream?.flush()
+    }
+
+    open val isDead: Boolean
+        get() = process?.isAlive?.not() ?: true
+
+    open fun destroy() {
+        process?.destroy()
+    }
+}
