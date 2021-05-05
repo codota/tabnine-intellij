c07e249e578aaa89c67bd261e2b6fbba92d3cfc1
Dima <dima@codota.com>
2021-05-05 18:38:56 +0300

    to kotlin

--- src/main/java/com/tabnine/Initializer.java
+++ src/main/java/com/tabnine/Initializer.java
@@ -5,19 +5,14 @@ import com.intellij.ide.ApplicationLoadListener;
 import com.intellij.ide.plugins.PluginInstaller;
 import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.openapi.application.Application;
-import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.project.Project;
 import com.intellij.util.messages.MessageBusConnection;
-import com.tabnine.config.Config;
-import com.tabnine.logging.SentryAppender;
 import com.tabnine.lifecycle.BinaryNotificationsLifecycle;
 import com.tabnine.lifecycle.BinaryPromotionStatusBarLifecycle;
 import com.tabnine.lifecycle.TabNineDisablePluginListener;
-import com.tabnine.logging.TabnineFilter;
-import org.apache.log4j.LogManager;
+import com.tabnine.logging.LogInitializerKt;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
-import io.sentry.Sentry;
 
 import static com.tabnine.general.DependencyContainer.*;
 
@@ -35,7 +30,7 @@ public class Initializer implements ApplicationLoadListener, AppLifecycleListene
 
     @Override
     public void appStarting(@Nullable Project projectFromCommandLine) {
-        initLogger();
+        LogInitializerKt.init();
         listener = singletonOfTabNineDisablePluginListener();
         binaryNotificationsLifecycle = instanceOfBinaryNotifications();
         binaryPromotionStatusBarLifecycle = instanceOfBinaryPromotionStatusBar();
@@ -43,19 +38,5 @@ public class Initializer implements ApplicationLoadListener, AppLifecycleListene
         PluginInstaller.addStateListener(instanceOfTabNinePluginStateListener());
         binaryNotificationsLifecycle.poll();
         binaryPromotionStatusBarLifecycle.poll();
-    }
-
-    private void initLogger() {
-        Sentry.init();
-        Sentry.configureScope(scope -> {
-            scope.setTag("ide", ApplicationInfo.getInstance().getVersionName());
-            scope.setTag("ideVersion", ApplicationInfo.getInstance().getFullVersion());
-            scope.setTag("os", System.getProperty("os.name"));
-            scope.setTag("channel", Config.CHANNEL);
-        });
-        org.apache.log4j.Logger rootLogger = LogManager.getRootLogger();
-        SentryAppender sentryAppender = new SentryAppender();
-        sentryAppender.addFilter(new TabnineFilter());
-        rootLogger.addAppender(sentryAppender);
     }
 }
---
+++ src/main/java/com/tabnine/logging/LogInitializer.kt
@@ -0,0 +1,21 @@
+package com.tabnine.logging
+
+import com.intellij.openapi.application.ApplicationInfo
+import com.tabnine.config.Config
+import io.sentry.Scope
+import io.sentry.Sentry
+import org.apache.log4j.LogManager
+
+fun init() {
+    Sentry.init()
+    Sentry.configureScope { scope: Scope ->
+        scope.setTag("ide", ApplicationInfo.getInstance().versionName)
+        scope.setTag("ideVersion", ApplicationInfo.getInstance().fullVersion)
+        scope.setTag("os", System.getProperty("os.name"))
+        scope.setTag("channel", Config.CHANNEL)
+    }
+    val rootLogger = LogManager.getRootLogger()
+    val sentryAppender = SentryAppender()
+    sentryAppender.addFilter(TabnineFilter())
+    rootLogger.addAppender(sentryAppender)
+}
