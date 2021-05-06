abc3db21bcb8762bc6ea5a0d77b729e39d65cc50
Dima <60742964+dimacodota@users.noreply.github.com>
2021-05-06 15:19:52 +0300

    sentry install id (#143)
    * sentry install id
    
    * trigger test

--- src/main/java/com/tabnine/logging/LogInitializer.kt
+++ src/main/java/com/tabnine/logging/LogInitializer.kt
@@ -1,14 +1,19 @@
 package com.tabnine.logging
 
 import com.intellij.openapi.application.ApplicationInfo
+import com.intellij.openapi.application.PermanentInstallationID
 import com.tabnine.config.Config
 import com.tabnine.general.Utils
 import io.sentry.Scope
 import io.sentry.Sentry
+import io.sentry.protocol.User
 import org.apache.log4j.LogManager
 
 fun init() {
     Sentry.init()
+    if (!Sentry.isEnabled()) {
+        return
+    }
     Sentry.configureScope { scope: Scope ->
         scope.setTag("ide", ApplicationInfo.getInstance().versionName)
         scope.setTag("ideVersion", ApplicationInfo.getInstance().fullVersion)
@@ -16,6 +21,10 @@ fun init() {
         scope.setTag("os", System.getProperty("os.name"))
         scope.setTag("channel", Config.CHANNEL)
     }
+    val user = User().apply {
+        id = PermanentInstallationID.get()
+    }
+    Sentry.setUser(user)
     val tabnineLogger = LogManager.getLogger("#com.tabnine")
     val sentryAppender = SentryAppender()
     tabnineLogger.addAppender(sentryAppender)
