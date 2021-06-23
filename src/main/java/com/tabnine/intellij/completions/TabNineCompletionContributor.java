ff152c694d153f443b3c464f3a011a1fdc2f3e4a
Eran Shoval <erans@codota.com>
2021-06-23 10:25:51 +0300

    Adding auto import

--- src/main/java/com/tabnine/intellij/completions/TabNineCompletionContributor.java
+++ src/main/java/com/tabnine/intellij/completions/TabNineCompletionContributor.java
@@ -1,11 +1,35 @@
 package com.tabnine.intellij.completions;
 
 import com.intellij.codeInsight.completion.*;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInsight.daemon.impl.ShowIntentionsPass;
+import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler;
 import com.intellij.codeInsight.lookup.*;
+import com.intellij.codeInsight.lookup.impl.LookupImpl;
+import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.Disposable;
 import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
+import com.intellij.openapi.command.CommandProcessor;
 import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.editor.ex.MarkupModelEx;
+import com.intellij.openapi.editor.ex.RangeHighlighterEx;
+import com.intellij.openapi.editor.ex.util.EditorUtil;
+import com.intellij.openapi.editor.impl.CaretModelImpl;
+import com.intellij.openapi.editor.impl.EditorImpl;
+import com.intellij.openapi.editor.impl.event.MarkupModelListener;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
 import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.ObjectUtils;
 import com.intellij.util.messages.MessageBus;
 import com.tabnine.binary.requests.autocomplete.AutocompleteResponse;
 import com.tabnine.binary.requests.autocomplete.ResultEntry;
@@ -16,11 +40,15 @@ import com.tabnine.prediction.CompletionFacade;
 import com.tabnine.prediction.TabNineCompletion;
 import com.tabnine.prediction.TabNinePrefixMatcher;
 import com.tabnine.prediction.TabNineWeigher;
+import com.tabnine.selections.AutoImporter;
 import com.tabnine.selections.TabNineLookupListener;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 import static com.tabnine.general.StaticConfig.*;
 import static com.tabnine.general.Utils.endsWithADot;
@@ -140,18 +168,109 @@ public class TabNineCompletionContributor extends CompletionContributor {
             }
             return lookupElement;
         } else {
-            lookupElementBuilder = lookupElementBuilder.withInsertHandler((context, item) -> {
+      lookupElementBuilder =
+          lookupElementBuilder.withInsertHandler(
+              (context, item) -> {
+                final Project project = context.getProject();
+                final Document document = context.getDocument();
+                final Editor editor = context.getEditor();
+                final PsiFile file = context.getFile();
                 int end = context.getTailOffset();
+                int start = context.getStartOffset();
                 TabNineCompletion lookupElement = (TabNineCompletion) item.getObject();
                 try {
-                    context.getDocument().insertString(end + lookupElement.oldSuffix.length(), lookupElement.newSuffix);
-                    context.getDocument().deleteString(end, end + lookupElement.oldSuffix.length());
-                } catch(RuntimeException re) {
-                    Logger.getInstance(getClass()).warn("Error inserting new suffix. End = " + end +
-                            ", old suffix length = " + lookupElement.oldSuffix.length() + ", new suffix length = "
-                            + lookupElement.newSuffix.length(), re);
+                  document.insertString(
+                      end + lookupElement.oldSuffix.length(), lookupElement.newSuffix);
+                  document.deleteString(end, end + lookupElement.oldSuffix.length());
+                  System.out.println(
+                      "--> Context start is " + start + " and context end is " + end);
+                  System.out.println("--> Current offset is " + editor.getCaretModel().getOffset());
+                  System.out.println(
+                      "--> Total length is "
+                          + (lookupElement.newPrefix + lookupElement.newSuffix).length());
+                  System.out.println(
+                      "--> Computed start is "
+                          + (end - lookupElement.newPrefix.length())
+                          + " and computed end is "
+                          + (end + lookupElement.newSuffix.length()));
+
+                  Key<Boolean> myKey = Key.create("eransho");
+                  Boolean eransho = editor.getUserData(myKey);
+                  Disposable myDisposable = Disposer.newDisposable();
+                  EditorUtil.disposeWithEditor(editor, myDisposable);
+
+                  AutoImporter.registerTabNineAutoImporter(context, item);
+
+                  //                    String insertedText = document.getText(new TextRange(start,
+                  // end));
+                  //                    Set<String> termsSet =
+                  // Arrays.stream(insertedText.split("\\w+")).collect(Collectors.toSet());
+                  //
+                  //                    if (eransho == null || eransho == Boolean.FALSE) {
+                  //                        ((MarkupModelEx) ((EditorEx)
+                  // editor).getFilteredDocumentMarkupModel()).addMarkupModelListener(myDisposable,
+                  // new MarkupModelListener() {
+                  //                            @Override
+                  //                            public void afterAdded(@NotNull RangeHighlighterEx
+                  // highlighter) {
+                  //                                Object errorTooltip =
+                  // highlighter.getErrorStripeTooltip();
+                  //                                HighlightInfo highlightInfo =
+                  // ObjectUtils.tryCast(errorTooltip, HighlightInfo.class);
+                  //                                if (highlightInfo == null ||
+                  // highlightInfo.getSeverity() != HighlightSeverity.ERROR) {
+                  //                                    return;
+                  //                                }
+                  //                                System.out.println("--> highlighter is " +
+                  // highlighter);
+                  //                                System.out.println("--> lookupElement is " +
+                  // lookupElement.newPrefix + ", " + lookupElement.newSuffix + " and " +
+                  // lookupElement.oldPrefix + ", " + lookupElement.oldSuffix);
+                  //                                if (start <=
+                  // highlighter.getAffectedAreaStartOffset() && end >=
+                  // highlighter.getAffectedAreaEndOffset()) {
+                  //                                    final DaemonCodeAnalyzerImpl codeAnalyzer =
+                  // (DaemonCodeAnalyzerImpl) DaemonCodeAnalyzer.getInstance(project);
+                  //
+                  // CommandProcessor.getInstance().runUndoTransparentAction(() ->
+                  // codeAnalyzer.autoImportReferenceAtCursor(editor, file));
+                  //
+                  // List<HighlightInfo.IntentionActionDescriptor> availableFixes =
+                  // ShowIntentionsPass.getAvailableFixes(editor, file, -1,
+                  // highlighter.getEndOffset());
+                  //
+                  // List<HighlightInfo.IntentionActionDescriptor> filteredFixes =
+                  // availableFixes.stream().filter(f ->
+                  // f.getAction().getText().toLowerCase().contains("import")).collect(Collectors.toList());
+                  //                                    System.out.println(filteredFixes);
+                  //
+                  ////                                    availableFixes.stream().filter(f ->
+                  // f.getAction().getText().toLowerCase().contains("import")).findFirst().ifPresent(fix -> {
+                  ////
+                  // PsiDocumentManager.getInstance(project).commitDocument(highlighter.getDocument());
+                  ////
+                  // ShowIntentionActionsHandler.chooseActionAndInvoke(file, editor,
+                  // fix.getAction(), fix.getAction().getText());
+                  ////                                        Disposer.dispose(myDisposable);
+                  ////                                    });
+                  //                                }
+                  //                            }
+                  //                        });
+                  //                        editor.putUserData(myKey, Boolean.TRUE);
+                  //                    }
+
+                } catch (RuntimeException re) {
+                  Logger.getInstance(getClass())
+                      .warn(
+                          "Error inserting new suffix. End = "
+                              + end
+                              + ", old suffix length = "
+                              + lookupElement.oldSuffix.length()
+                              + ", new suffix length = "
+                              + lookupElement.newSuffix.length(),
+                          re);
                 }
-            });
+              });
         }
         return lookupElementBuilder;
     }
---
+++ src/main/java/com/tabnine/selections/AutoImporter.java
@@ -0,0 +1,186 @@
+package com.tabnine.selections;
+
+import com.intellij.codeInsight.completion.InsertionContext;
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
+import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl;
+import com.intellij.codeInsight.daemon.impl.HighlightInfo;
+import com.intellij.codeInsight.daemon.impl.ShowIntentionsPass;
+import com.intellij.codeInsight.intention.impl.ShowIntentionActionsHandler;
+import com.intellij.codeInsight.lookup.LookupElement;
+import com.intellij.openapi.Disposable;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.WriteAction;
+import com.intellij.openapi.command.CommandProcessor;
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.ex.EditorEx;
+import com.intellij.openapi.editor.ex.RangeHighlighterEx;
+import com.intellij.openapi.editor.ex.util.EditorUtil;
+import com.intellij.openapi.editor.impl.event.MarkupModelListener;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.openapi.util.Key;
+import com.intellij.openapi.util.Pair;
+import com.intellij.openapi.util.TextRange;
+import com.intellij.psi.PsiDocumentManager;
+import com.intellij.psi.PsiFile;
+import com.intellij.util.ObjectUtils;
+import org.jetbrains.annotations.NotNull;
+import com.intellij.openapi.diagnostic.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AutoImporter implements MarkupModelListener {
+
+  private static final Key<AutoImporter> TABNINE_AUTO_IMPORTER_KEY =
+      Key.create("TABNINE_AUTO_IMPORTER");
+
+  private int startOffset;
+  private int endOffset;
+  @NotNull private final Project project;
+  @NotNull Editor editor;
+  private Disposable myDisposable;
+  private final Map<String, Pair<Integer, Boolean>> possibleRequiredImports = new HashMap<>();
+  private final List<HighlightInfo.IntentionActionDescriptor> importFixes = new ArrayList<>();
+
+  private AutoImporter(@NotNull InsertionContext context) {
+    project = context.getProject();
+    editor = context.getEditor();
+  }
+
+  private void init(@NotNull InsertionContext context) {
+    myDisposable = Disposer.newDisposable();
+    EditorUtil.disposeWithEditor(editor, myDisposable);
+    ((EditorEx) editor).getFilteredDocumentMarkupModel().addMarkupModelListener(myDisposable, this);
+
+    startOffset = context.getStartOffset();
+    endOffset = context.getTailOffset();
+    String insertedText = context.getDocument().getText(new TextRange(startOffset, endOffset));
+    int index = 0;
+    for (String term : insertedText.split("\\W+")) {
+      if (possibleRequiredImports.containsKey(term)) {
+        continue;
+      }
+      possibleRequiredImports.put(term, Pair.create(++index, false));
+    }
+    System.out.println("possibleRequiredImports: " + possibleRequiredImports);
+  }
+
+  public static void registerTabNineAutoImporter(
+      @NotNull InsertionContext context, @NotNull LookupElement element) {
+    Editor editor = context.getEditor();
+    AutoImporter autoImporter = editor.getUserData(TABNINE_AUTO_IMPORTER_KEY);
+    if (autoImporter == null) {
+      autoImporter = new AutoImporter(context);
+      editor.putUserData(TABNINE_AUTO_IMPORTER_KEY, autoImporter);
+    } else {
+      autoImporter.cleanup();
+    }
+    autoImporter.init(context);
+  }
+
+  @Override
+  public void afterAdded(@NotNull RangeHighlighterEx highlighter) {
+    if (startOffset > highlighter.getAffectedAreaStartOffset()
+        || endOffset < highlighter.getAffectedAreaEndOffset()) {
+      return;
+    }
+    final Document document = highlighter.getDocument();
+    final PsiFile file = PsiDocumentManager.getInstance(project).getPsiFile(document);
+    if (file == null) {
+      return;
+    }
+    Object errorTooltip = highlighter.getErrorStripeTooltip();
+    HighlightInfo highlightInfo = ObjectUtils.tryCast(errorTooltip, HighlightInfo.class);
+    if (highlightInfo == null) {
+      return;
+    }
+    String highlightedText = highlightInfo.getText();
+    if (!isRelevant(highlightedText)) {
+      if (isLastToVisit(highlightedText)) {
+        // Last one - invoke actions and cleanup
+        invokeImportActions(file);
+      }
+      return;
+    }
+
+    System.out.println("--> highlighter is " + highlighter);
+
+    // Try auto import
+    final DaemonCodeAnalyzerImpl codeAnalyzer =
+        (DaemonCodeAnalyzerImpl) DaemonCodeAnalyzer.getInstance(project);
+    CommandProcessor.getInstance()
+        .runUndoTransparentAction(() -> codeAnalyzer.autoImportReferenceAtCursor(editor, file));
+    // Collect import fixes
+    List<HighlightInfo.IntentionActionDescriptor> availableFixes =
+        ShowIntentionsPass.getAvailableFixes(editor, file, -1, highlighter.getEndOffset());
+
+
+    System.out.println("--> availableFixes for " + highlightedText + ": " + availableFixes);
+
+
+    availableFixes.stream()
+        .filter(f -> f.getAction().getText().toLowerCase().contains("import"))
+        .findFirst()
+        .ifPresent(importFixes::add);
+    markAsVisited(highlightedText);
+    if (isLastToVisit(highlightedText)) {
+      // Last one - invoke actions and cleanup
+      invokeImportActions(file);
+    }
+
+    System.out.println("--> finish afterAdded()");
+  }
+
+  private boolean isRelevant(String term) {
+    Pair<Integer, Boolean> pair = possibleRequiredImports.get(term);
+    if (pair == null) {
+      return false;
+    }
+    return !pair.getSecond();
+  }
+
+  private boolean isLastToVisit(String term) {
+    Pair<Integer, Boolean> pair = possibleRequiredImports.get(term);
+    return pair != null && pair.getFirst() == possibleRequiredImports.size();
+  }
+
+  private void markAsVisited(String term) {
+    possibleRequiredImports.computeIfPresent(term, (k, v) -> Pair.create(v.getFirst(), true));
+  }
+
+  private void invokeImportActions(@NotNull PsiFile file) {
+    System.out.println("--> About to invoke import actions from: " + importFixes);
+    try {
+      WriteAction.run(() -> {
+        importFixes.forEach(
+                fix -> ShowIntentionActionsHandler.chooseActionAndInvoke(
+                        file, editor, fix.getAction(), fix.getAction().getText()));
+      });
+    } catch (Throwable e) {
+      Logger.getInstance(getClass()).warn("Failed to auto import", e);
+    } finally {
+      cleanup();
+    }
+//    importFixes.stream().findFirst().ifPresent(
+//            fix -> ShowIntentionActionsHandler.chooseActionAndInvoke(
+//                    file, editor, fix.getAction(), fix.getAction().getText()));
+
+  }
+
+  private void cleanup() {
+    unregisteredAsMarkupModelListener();
+    possibleRequiredImports.clear();
+    importFixes.clear();
+  }
+
+  private void unregisteredAsMarkupModelListener() {
+    if (!Disposer.isDisposed(myDisposable)) {
+      Disposer.dispose(myDisposable);
+    }
+  }
+}
