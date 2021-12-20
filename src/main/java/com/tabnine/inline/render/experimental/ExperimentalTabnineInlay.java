26695ac93279ebbcb1953a9de4949332d7e77f7c
yonip23 <yoni@tabnine.com>
2021-12-20 18:30:30 +0200

    handle another edge case + fix test

--- src/main/java/com/tabnine/inline/render/experimental/ExperimentalTabnineInlay.java
+++ src/main/java/com/tabnine/inline/render/experimental/ExperimentalTabnineInlay.java
@@ -14,13 +14,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class ExperimentalTabnineInlay implements TabnineInlay {
-    private GenericInlayWrapper inline;
+    private GenericInlayWrapper inlineBeforeSuffix;
+    private GenericInlayWrapper inlineAfterSuffix;
     private GenericInlayWrapper block;
 
     @Nullable
     public Integer getOffset() {
-        if (inline != null) {
-            return inline.inner().getOffset();
+        if (inlineBeforeSuffix != null) {
+            return inlineBeforeSuffix.inner().getOffset();
         }
         if (block != null) {
             return block.inner().getOffset();
@@ -31,8 +32,8 @@ public class ExperimentalTabnineInlay implements TabnineInlay {
 
     @Nullable
     public Rectangle getBounds() {
-        if (inline != null) {
-            return inline.inner().getBounds();
+        if (inlineBeforeSuffix != null) {
+            return inlineBeforeSuffix.inner().getBounds();
         }
         if (block != null) {
             return block.inner().getBounds();
@@ -42,12 +43,15 @@ public class ExperimentalTabnineInlay implements TabnineInlay {
     }
 
     public boolean isEmpty() {
-        return this.inline == null && this.block == null;
+        return this.inlineBeforeSuffix == null && this.inlineAfterSuffix != null && this.block == null;
     }
 
     public void register(Disposable parent) {
-        if (inline != null) {
-            Disposer.register(parent, inline.inner());
+        if (inlineBeforeSuffix != null) {
+            Disposer.register(parent, inlineBeforeSuffix.inner());
+        }
+        if (inlineAfterSuffix != null) {
+            Disposer.register(parent, inlineAfterSuffix.inner());
         }
         if (block != null) {
             Disposer.register(parent, block.inner());
@@ -55,9 +59,14 @@ public class ExperimentalTabnineInlay implements TabnineInlay {
     }
 
     public void clear() {
-        if (inline != null) {
-            Disposer.dispose(inline.inner());
-            inline = null;
+        if (inlineBeforeSuffix != null) {
+            Disposer.dispose(inlineBeforeSuffix.inner());
+            inlineBeforeSuffix = null;
+        }
+
+        if (inlineAfterSuffix != null) {
+            Disposer.dispose(inlineAfterSuffix.inner());
+            inlineAfterSuffix = null;
         }
 
         if (block != null) {
@@ -72,11 +81,32 @@ public class ExperimentalTabnineInlay implements TabnineInlay {
         List<String> otherLines = lines.stream().skip(1).collect(Collectors.toList());
 
         if (!firstLine.isEmpty()) {
+            int endIndex = firstLine.indexOf(completion.oldSuffix);
+            if (!completion.oldSuffix.isEmpty() && endIndex >= 0) {
+                String before = firstLine.substring(0, endIndex);
+                InlineElementRenderer beforeInline =
+                        new InlineElementRenderer(editor, before, completion.deprecated);
+                this.inlineBeforeSuffix = new GenericInlayWrapper(editor
+                        .getInlayModel()
+                        .addInlineElement(offset, true, beforeInline));
+
+                int startOfAfter = endIndex + completion.oldSuffix.length();
+                if (startOfAfter < firstLine.length()) {
+                    String after = firstLine.substring(startOfAfter);
+
+                    InlineElementRenderer afterInline =
+                            new InlineElementRenderer(editor, after, completion.deprecated);
+                    this.inlineAfterSuffix = new GenericInlayWrapper(editor
+                            .getInlayModel()
+                            .addInlineElement(offset + before.length(), true, afterInline));
+                }
+            } else {
             InlineElementRenderer inlineElementRenderer =
                     new InlineElementRenderer(editor, firstLine, completion.deprecated);
-            this.inline = new GenericInlayWrapper(editor
+            this.inlineBeforeSuffix = new GenericInlayWrapper(editor
                     .getInlayModel()
                     .addInlineElement(offset, true, inlineElementRenderer));
+            }
         }
         if (otherLines.size() > 0) {
             BlockElementRenderer blockElementRenderer =
