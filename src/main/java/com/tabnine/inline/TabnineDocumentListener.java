a513117b7e77eae83f18038cb848e09c83d79647
yonip23 <yoni@tabnine.com>
2021-12-28 17:16:46 +0200

    fix inline completions after opening brackets

--- src/main/java/com/tabnine/inline/TabnineDocumentListener.java
+++ src/main/java/com/tabnine/inline/TabnineDocumentListener.java
@@ -12,6 +12,7 @@ import com.intellij.openapi.editor.event.DocumentEvent;
 import com.intellij.openapi.editor.event.DocumentListener;
 import com.intellij.openapi.editor.ex.DocumentEx;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.wm.IdeFocusManager;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiFile;
@@ -21,69 +22,87 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.awt.*;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class TabnineDocumentListener implements DocumentListener {
 
-  private final InlineCompletionHandler handler = new InlineCompletionHandler(true);
+    private final InlineCompletionHandler handler = new InlineCompletionHandler(true);
 
-  private static final AtomicBoolean isMuted = new AtomicBoolean(false);
+    private static final java.util.List<String> AUTO_FILLING_PAIRS = Arrays.asList("()", "{}", "[]", "''", "\"\"");
 
-  @Override
-  public void documentChanged(@NotNull DocumentEvent event) {
-    if (isMuted.get()
-        || SuggestionsMode.getSuggestionMode() != SuggestionsMode.INLINE
-        || event.getNewFragment().toString().equals(CompletionUtil.DUMMY_IDENTIFIER)
-        || event.getNewLength() < 1) {
-      return;
-    }
-    Document document = event.getDocument();
-    if (ObjectUtils.doIfCast(document, DocumentEx.class, DocumentEx::isInBulkUpdate)
-        == Boolean.TRUE) {
-      return;
-    }
-    Editor editor = getActiveEditor(document);
+    private static final AtomicBoolean isMuted = new AtomicBoolean(false);
 
-    if( editor != null && !editor.getEditorKind() .equals(EditorKind.MAIN_EDITOR) && !ApplicationManager.getApplication().isUnitTestMode()) {
-      return;
+    @Override
+    public void documentChanged(@NotNull DocumentEvent event) {
+        if (isMuted.get()
+                || SuggestionsMode.getSuggestionMode() != SuggestionsMode.INLINE
+                || event.getNewFragment().toString().equals(CompletionUtil.DUMMY_IDENTIFIER)
+                || event.getNewLength() < 1) {
+            return;
+        }
+        Document document = event.getDocument();
+
+        if (ObjectUtils.doIfCast(document, DocumentEx.class, DocumentEx::isInBulkUpdate)
+                == Boolean.TRUE) {
+            return;
+        }
+        Editor editor = getActiveEditor(document);
+
+        if (editor != null && !editor.getEditorKind().equals(EditorKind.MAIN_EDITOR) && !ApplicationManager.getApplication().isUnitTestMode()) {
+            return;
+        }
+
+        Project project = ObjectUtils.doIfNotNull(editor, Editor::getProject);
+        PsiFile file =
+                ObjectUtils.doIfNotNull(
+                        project, proj -> PsiDocumentManager.getInstance(proj).getPsiFile(document));
+        if (editor != null) {
+            CompletionPreview.disposeIfExists(editor, preview -> event.getNewLength() > 1);
+        }
+        if (editor == null || project == null || file == null) {
+            return;
+        }
+
+        int startOffset = event.getOffset();
+        int endOffset = event.getOffset() + event.getNewLength();
+        int invocationOffset = endOffset;
+
+        if (startOffset > 0 && startOffset < endOffset) {
+            String textIncludingPreviousChar = document.getText(new TextRange(startOffset - 1, endOffset));
+
+            if (AUTO_FILLING_PAIRS.contains(textIncludingPreviousChar)) {
+                // call `invoke` with the previous char again to show completion before the auto-filled char.
+                invocationOffset = startOffset;
+            }
+        }
+
+        handler.invoke(project, editor, file, invocationOffset);
     }
 
-    Project project = ObjectUtils.doIfNotNull(editor, Editor::getProject);
-    PsiFile file =
-        ObjectUtils.doIfNotNull(
-            project, proj -> PsiDocumentManager.getInstance(proj).getPsiFile(document));
-    if (editor != null) {
-      CompletionPreview.disposeIfExists(editor, preview -> event.getNewLength() > 1);
+    public static void mute() {
+        isMuted.set(true);
     }
-    if (editor == null || project == null || file == null) {
-      return;
-    }
-    handler.invoke(project, editor, file, event.getOffset() + event.getNewLength());
-  }
 
-  public static void mute() {
-    isMuted.set(true);
-  }
-
-  public static void unmute() {
-    isMuted.set(false);
-  }
-
-  @Nullable
-  private static Editor getActiveEditor(@NotNull Document document) {
-    if (!ApplicationManager.getApplication().isDispatchThread()) {
-      return null;
+    public static void unmute() {
+        isMuted.set(false);
     }
-    Component focusOwner = IdeFocusManager.getGlobalInstance().getFocusOwner();
-    DataContext dataContext = DataManager.getInstance().getDataContext(focusOwner);
-    // ignore caret placing when exiting
-    Editor activeEditor =
-        ApplicationManager.getApplication().isDisposed()
-            ? null
-            : CommonDataKeys.EDITOR.getData(dataContext);
-    if (activeEditor != null && activeEditor.getDocument() != document) {
-      activeEditor = null;
+
+    @Nullable
+    private static Editor getActiveEditor(@NotNull Document document) {
+        if (!ApplicationManager.getApplication().isDispatchThread()) {
+            return null;
+        }
+        Component focusOwner = IdeFocusManager.getGlobalInstance().getFocusOwner();
+        DataContext dataContext = DataManager.getInstance().getDataContext(focusOwner);
+        // ignore caret placing when exiting
+        Editor activeEditor =
+                ApplicationManager.getApplication().isDisposed()
+                        ? null
+                        : CommonDataKeys.EDITOR.getData(dataContext);
+        if (activeEditor != null && activeEditor.getDocument() != document) {
+            activeEditor = null;
+        }
+        return activeEditor;
     }
-    return activeEditor;
-  }
 }
