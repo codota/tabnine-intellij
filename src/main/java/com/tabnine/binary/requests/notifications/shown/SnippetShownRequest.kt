cb2d65fd70a3b8547a50a0438233b291524708a2
yonip23 <yoni@tabnine.com>
2022-01-13 14:09:55 +0200

    send snippet shown event

---
+++ src/main/java/com/tabnine/binary/requests/notifications/shown/SnippetShownRequest.kt
@@ -0,0 +1,19 @@
+package com.tabnine.binary.requests.notifications.shown
+
+import com.tabnine.binary.BinaryRequest
+import com.tabnine.binary.requests.selection.SetStateBinaryResponse
+import com.tabnine.general.StaticConfig
+
+class SnippetShownRequest : BinaryRequest<SetStateBinaryResponse> {
+    override fun response(): Class<SetStateBinaryResponse> {
+        return SetStateBinaryResponse::class.java
+    }
+
+    override fun serialize(): Any {
+        return mapOf("SetState" to mapOf("state_type" to mapOf("SnippetShown" to this)))
+    }
+
+    override fun validate(response: SetStateBinaryResponse): Boolean {
+        return StaticConfig.SET_STATE_RESPONSE_RESULT_STRING == response.result
+    }
+}
