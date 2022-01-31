6f2012f1b0785b23bd957055a3bbb9acf093c8b2
yonip23 <yoni@tabnine.com>
2022-01-31 14:14:58 +0200

    making the plugin compatible with 2019.3 - latest eap

--- src/main/java/com/tabnine/Initializer.java
+++ src/main/java/com/tabnine/Initializer.java
@@ -1,37 +1,24 @@
 package com.tabnine;
 
-import com.intellij.ide.AppLifecycleListener;
-import com.intellij.ide.ApplicationLoadListener;
+import com.intellij.ide.ApplicationInitializedListener;
 import com.intellij.ide.plugins.PluginInstaller;
 import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.openapi.application.Application;
-import com.intellij.openapi.project.Project;
-import com.intellij.util.messages.MessageBusConnection;
 import com.tabnine.capabilities.CapabilitiesService;
 import com.tabnine.lifecycle.BinaryNotificationsLifecycle;
 import com.tabnine.lifecycle.BinaryPromotionStatusBarLifecycle;
 import com.tabnine.lifecycle.TabNineDisablePluginListener;
 import com.tabnine.lifecycle.TabnineUpdater;
 import com.tabnine.logging.LogInitializerKt;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 import static com.tabnine.general.DependencyContainer.*;
 
-public class Initializer implements ApplicationLoadListener, AppLifecycleListener {
+public class Initializer implements ApplicationInitializedListener {
     private TabNineDisablePluginListener listener;
     private BinaryNotificationsLifecycle binaryNotificationsLifecycle;
     private BinaryPromotionStatusBarLifecycle binaryPromotionStatusBarLifecycle;
 
     @Override
-    public void beforeApplicationLoaded(@NotNull Application application, @NotNull String configPath) {
-        final MessageBusConnection connection = application.getMessageBus().connect();
-
-        connection.subscribe(AppLifecycleListener.TOPIC, this);
-    }
-
-    @Override
-    public void appStarting(@Nullable Project projectFromCommandLine) {
+    public void componentsInitialized() {
         LogInitializerKt.init();
         listener = singletonOfTabNineDisablePluginListener();
         binaryNotificationsLifecycle = instanceOfBinaryNotifications();
@@ -42,6 +29,5 @@ public class Initializer implements ApplicationLoadListener, AppLifecycleListene
         binaryPromotionStatusBarLifecycle.poll();
         CapabilitiesService.getInstance().init();
         TabnineUpdater.pollUpdates();
-
     }
 }
