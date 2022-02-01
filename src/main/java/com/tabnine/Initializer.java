2c703fda2389dfe1d361cfd239454a51e76ed455
yonip23 <67855609+yonip23@users.noreply.github.com>
2022-02-01 13:34:11 +0200

    making the plugin compatible with 2019.3 - latest eap (#275)
    * making the plugin compatible with 2019.3 - latest eap
    
    * oh well
    
    * verify latest eap job
    
    * latest eap -> nightly job
    
    * make compatibility-stable run on earliest & latest
    
    * added slack notification
    
    * check notification

--- src/main/java/com/tabnine/Initializer.java
+++ src/main/java/com/tabnine/Initializer.java
@@ -1,12 +1,11 @@
 package com.tabnine;
 
-import com.intellij.ide.AppLifecycleListener;
-import com.intellij.ide.ApplicationLoadListener;
 import com.intellij.ide.plugins.PluginInstaller;
 import com.intellij.ide.plugins.PluginManagerCore;
-import com.intellij.openapi.application.Application;
+import com.intellij.openapi.application.PreloadingActivity;
+import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.project.Project;
-import com.intellij.util.messages.MessageBusConnection;
+import com.intellij.openapi.startup.StartupActivity;
 import com.tabnine.capabilities.CapabilitiesService;
 import com.tabnine.lifecycle.BinaryNotificationsLifecycle;
 import com.tabnine.lifecycle.BinaryPromotionStatusBarLifecycle;
@@ -14,34 +13,39 @@ import com.tabnine.lifecycle.TabNineDisablePluginListener;
 import com.tabnine.lifecycle.TabnineUpdater;
 import com.tabnine.logging.LogInitializerKt;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+
+import java.util.concurrent.atomic.AtomicBoolean;
 
 import static com.tabnine.general.DependencyContainer.*;
 
-public class Initializer implements ApplicationLoadListener, AppLifecycleListener {
+public class Initializer extends PreloadingActivity implements StartupActivity {
     private TabNineDisablePluginListener listener;
     private BinaryNotificationsLifecycle binaryNotificationsLifecycle;
     private BinaryPromotionStatusBarLifecycle binaryPromotionStatusBarLifecycle;
+    private final AtomicBoolean initialized = new AtomicBoolean(false);
 
     @Override
-    public void beforeApplicationLoaded(@NotNull Application application, @NotNull String configPath) {
-        final MessageBusConnection connection = application.getMessageBus().connect();
-
-        connection.subscribe(AppLifecycleListener.TOPIC, this);
+    public void preload(@NotNull ProgressIndicator indicator) {
+        initialize();
     }
 
     @Override
-    public void appStarting(@Nullable Project projectFromCommandLine) {
-        LogInitializerKt.init();
-        listener = singletonOfTabNineDisablePluginListener();
-        binaryNotificationsLifecycle = instanceOfBinaryNotifications();
-        binaryPromotionStatusBarLifecycle = instanceOfBinaryPromotionStatusBar();
-        PluginManagerCore.addDisablePluginListener(listener::onDisable);
-        PluginInstaller.addStateListener(instanceOfTabNinePluginStateListener());
-        binaryNotificationsLifecycle.poll();
-        binaryPromotionStatusBarLifecycle.poll();
-        CapabilitiesService.getInstance().init();
-        TabnineUpdater.pollUpdates();
+    public void runActivity(@NotNull Project project) {
+        initialize();
+    }
 
+    private void initialize() {
+        if (!initialized.getAndSet(true)) {
+            LogInitializerKt.init();
+            listener = singletonOfTabNineDisablePluginListener();
+            binaryNotificationsLifecycle = instanceOfBinaryNotifications();
+            binaryPromotionStatusBarLifecycle = instanceOfBinaryPromotionStatusBar();
+            PluginManagerCore.addDisablePluginListener(listener::onDisable);
+            PluginInstaller.addStateListener(instanceOfTabNinePluginStateListener());
+            binaryNotificationsLifecycle.poll();
+            binaryPromotionStatusBarLifecycle.poll();
+            CapabilitiesService.getInstance().init();
+            TabnineUpdater.pollUpdates();
+        }
     }
 }
