4d124103c1e8f1429262ba030d719bccfdb6d450
yonip23 <67855609+yonip23@users.noreply.github.com>
2022-03-21 16:50:24 +0200

    use api version 4.1.21 (#297)
    * use api version 4.1.21
    
    * fix formatting

---
+++ src/main/java/com/tabnine/binary/requests/autocomplete/UserIntent.kt
@@ -0,0 +1,12 @@
+package com.tabnine.binary.requests.autocomplete
+
+/**
+ * The intent which triggered a snippet completion request in the binary.
+ */
+enum class UserIntent {
+    Comment,
+    Block,
+    FunctionDeclaration,
+    NoScope,
+    NewLine,
+}
--- src/main/java/com/tabnine/general/StaticConfig.java
+++ src/main/java/com/tabnine/general/StaticConfig.java
@@ -22,7 +22,7 @@ public class StaticConfig {
   public static final String TABNINE_PLUGIN_ID_RAW = "com.tabnine.TabNine";
   public static final PluginId TABNINE_PLUGIN_ID = PluginId.getId(TABNINE_PLUGIN_ID_RAW);
   public static final int MAX_COMPLETIONS = 5;
-  public static final String BINARY_PROTOCOL_VERSION = "4.0.57";
+  public static final String BINARY_PROTOCOL_VERSION = "4.1.21";
   public static final int COMPLETION_TIME_THRESHOLD = 1000;
   public static final int NEWLINE_COMPLETION_TIME_THRESHOLD = 3000;
   public static final int ILLEGAL_RESPONSE_THRESHOLD = 5;
