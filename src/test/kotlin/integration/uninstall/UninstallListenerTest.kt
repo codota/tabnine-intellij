9a963e4dba597b2019b7312aab00fbe3d325c9f0
yonip23 <yoni@tabnine.com>
2022-05-15 09:52:42 +0300

    extract test driver

--- src/test/kotlin/integration/uninstall/UninstallListenerTest.kt
+++ src/test/kotlin/integration/uninstall/UninstallListenerTest.kt
@@ -2,119 +2,84 @@ package integration.uninstall
 
 import com.intellij.openapi.extensions.PluginId
 import com.tabnine.UninstallListener
-import com.tabnine.binary.BinaryRequestFacade
-import com.tabnine.binary.requests.uninstall.UninstallRequest
-import com.tabnine.binary.requests.uninstall.UninstallResponse
-import com.tabnine.general.readTempTabninePluginZip
-import com.tabnine.lifecycle.UninstallReporter
-import io.mockk.every
-import io.mockk.mockkStatic
 import org.junit.Test
-import org.mockito.ArgumentMatchers.any
-import org.mockito.ArgumentMatchers.anyMap
-import org.mockito.Mockito.`when`
-import org.mockito.Mockito.mock
-import org.mockito.Mockito.never
-import org.mockito.Mockito.times
-import org.mockito.Mockito.verify
 
 class UninstallListenerTest {
-    private val uninstallReporterMock: UninstallReporter = mock(UninstallReporter::class.java)
-    private var binaryRequestFacadeMock: BinaryRequestFacade = mock(BinaryRequestFacade::class.java)
+    private val driver = UninstallTestDriver()
     private val uninstallListener = UninstallListener(
-        binaryRequestFacadeMock,
-        uninstallReporterMock
+        driver.binaryRequestFacadeMock,
+        driver.uninstallReporterMock
     )
 
     @Test
     fun shouldNotReportUninstallWhenDescriptorIsNotOurs() {
         uninstallListener.uninstall(PluginDescriptorMock("0.0.0", PluginId.getId("kaka")))
 
-        verifyUninstallNotReported()
+        driver.verifyUninstallNotReported()
     }
 
     @Test
     fun shouldNotReportUninstallWhenZipVersionIsNewerThenCurrent() {
-        mockExistingPluginZipFiles(listOf("TabNine-0.0.1.jar"))
+        driver.mockExistingPluginZipFiles(listOf("TabNine-0.0.1.jar"))
 
         uninstallListener.uninstall(PluginDescriptorMock("0.0.0"))
 
-        verifyUninstallNotReported()
+        driver.verifyUninstallNotReported()
     }
 
     @Test
     fun shouldNotReportUninstallWhenZipHasOlderAndNewerVersions() {
-        mockExistingPluginZipFiles(listOf("TabNine-0.0.0.jar", "TabNine-0.0.2.jar"))
+        driver.mockExistingPluginZipFiles(listOf("TabNine-0.0.0.jar", "TabNine-0.0.2.jar"))
         uninstallListener.uninstall(PluginDescriptorMock("0.0.1"))
 
-        verifyUninstallNotReported()
+        driver.verifyUninstallNotReported()
     }
 
     @Test
     fun shouldFireUninstallRequestWhenZipVersionIsOlderThanCurrent() {
-        mockExistingPluginZipFiles(listOf("TabNine-0.0.0.jar"))
-        `when`(binaryRequestFacadeMock.executeRequest(any(UninstallRequest::class.java))).thenReturn(UninstallResponse())
+        driver.mockExistingPluginZipFiles(listOf("TabNine-0.0.0.jar"))
+        driver.mockUninstallResponse()
 
         uninstallListener.uninstall(PluginDescriptorMock("0.0.1"))
 
-        verifyUninstallRequestFired()
+        driver.verifyUninstallRequestFired()
     }
 
     @Test
     fun shouldFireUninstallRequestWhenZipDoesNotContainOurPlugin() {
-        mockExistingPluginZipFiles(listOf("NotTabNine-0.0.1.jar"))
-        `when`(binaryRequestFacadeMock.executeRequest(any(UninstallRequest::class.java))).thenReturn(UninstallResponse())
+        driver.mockExistingPluginZipFiles(listOf("NotTabNine-0.0.1.jar"))
+        driver.mockUninstallResponse()
 
         uninstallListener.uninstall(PluginDescriptorMock("0.0.0"))
 
-        verifyUninstallRequestFired()
+        driver.verifyUninstallRequestFired()
     }
 
     @Test
     fun shouldFireUninstallRequestWhenZipVersionIsNotAValidSemver() {
-        mockExistingPluginZipFiles(listOf("TabNine-0.a.1.jar"))
-        `when`(binaryRequestFacadeMock.executeRequest(any(UninstallRequest::class.java))).thenReturn(UninstallResponse())
+        driver.mockExistingPluginZipFiles(listOf("TabNine-0.a.1.jar"))
+        driver.mockUninstallResponse()
 
         uninstallListener.uninstall(PluginDescriptorMock("0.0.0"))
 
-        verifyUninstallRequestFired()
+        driver.verifyUninstallRequestFired()
     }
 
     @Test
     fun shouldFireUninstallRequestWhenZipVersionNotExists() {
-        mockExistingPluginZipFiles()
-        `when`(binaryRequestFacadeMock.executeRequest(any(UninstallRequest::class.java))).thenReturn(UninstallResponse())
+        driver.mockExistingPluginZipFiles()
+        driver.mockUninstallResponse()
 
         uninstallListener.uninstall(PluginDescriptorMock("0.0.1"))
 
-        verifyUninstallRequestFired()
+        driver.verifyUninstallRequestFired()
     }
 
     @Test
     fun shouldFallbackToUninstallReporterWhenFailedToSendRequest() {
-        mockExistingPluginZipFiles(listOf("TabNine-0.0.1.zip"))
+        driver.mockExistingPluginZipFiles(listOf("TabNine-0.0.1.zip"))
         uninstallListener.uninstall(PluginDescriptorMock("0.0.0"))
 
-        verifyUninstallReporterFallback()
-    }
-
-    private fun verifyUninstallNotReported() {
-        verify(binaryRequestFacadeMock, never()).executeRequest(any(UninstallRequest::class.java))
-        verify(uninstallReporterMock, never()).reportUninstall(anyMap())
-    }
-
-    private fun verifyUninstallRequestFired() {
-        verify(binaryRequestFacadeMock, times(1)).executeRequest(any(UninstallRequest::class.java))
-        verify(uninstallReporterMock, never()).reportUninstall(anyMap())
-    }
-
-    private fun verifyUninstallReporterFallback() {
-        verify(binaryRequestFacadeMock, times(1)).executeRequest(any(UninstallRequest::class.java))
-        verify(uninstallReporterMock, times(1)).reportUninstall(anyMap())
-    }
-
-    private fun mockExistingPluginZipFiles(filenames: List<String>? = null) {
-        mockkStatic("com.tabnine.general.PluginsZipReaderKt")
-        every { readTempTabninePluginZip() }.returns(filenames)
+        driver.verifyUninstallReporterFallback()
     }
 }
