25e5d93126dc4be20ea74e11a2ebd4639a574c14
yonip23 <yoni@tabnine.com>
2022-07-27 14:54:14 +0300

    fix type hint collision

--- src/main/java/com/tabnine/inline/render/InlineElementRenderer.kt
+++ src/main/java/com/tabnine/inline/render/InlineElementRenderer.kt
@@ -11,7 +11,6 @@ import java.awt.Rectangle
 
 class InlineElementRenderer(private val editor: Editor, private val suffix: String, private val deprecated: Boolean) :
     EditorCustomElementRenderer {
-//    private var renderingXAnchor: Int? = null
     private var color: Color? = null
     override fun calcWidthInPixels(inlay: Inlay<*>): Int {
         return editor.contentComponent
