25b3e33189a577c40df0d7a89f7e7c47fb0fed29
yonip23 <67855609+yonip23@users.noreply.github.com>
2022-08-02 11:42:25 +0300

    add snippet intent metadata (#354)

--- src/main/java/com/tabnine/binary/requests/autocomplete/SnippetContext.kt
+++ src/main/java/com/tabnine/binary/requests/autocomplete/SnippetContext.kt
@@ -3,5 +3,12 @@ package com.tabnine.binary.requests.autocomplete
 data class SnippetContext(
     val stop_reason: String?,
     val generated_tokens: Int?,
-    val user_intent: UserIntent
+    val user_intent: UserIntent,
+    val intent_metadata: SnippetIntentMetadata?
+)
+
+data class SnippetIntentMetadata(
+    val current_line_indentation: Int?,
+    val previous_line_indentation: Int?,
+    val triggered_after_character: Char?
 )
