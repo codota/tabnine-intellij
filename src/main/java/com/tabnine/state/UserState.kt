07ae1c62b9a989a778cca1140f0cbdad6fbf5be9
yonip23 <67855609+yonip23@users.noreply.github.com>
2022-08-03 15:24:35 +0300

    Fix NPE in user state (#357)
    * Rename .java to .kt
    
    * handle NPE in SuggestionHintState
    
    * make getInstance not nullable

---
+++ src/main/java/com/tabnine/state/UserState.kt
@@ -0,0 +1,32 @@
+package com.tabnine.state
+
+import com.tabnine.binary.requests.config.StateRequest
+import com.tabnine.general.DependencyContainer
+
+class UserState private constructor() {
+    val suggestionHintState: SuggestionHintState
+
+    init {
+        val binaryRequestFacade = DependencyContainer.instanceOfBinaryRequestFacade()
+        val stateResponse = binaryRequestFacade.executeRequest(StateRequest())
+        suggestionHintState = SuggestionHintState(stateResponse?.installationTime)
+    }
+
+    companion object {
+        var userState: UserState? = null
+
+        @JvmStatic
+        fun init() {
+            if (userState == null) {
+                userState = UserState()
+            }
+        }
+
+        @JvmStatic
+        val instance: UserState
+            get() {
+                init()
+                return userState!!
+            }
+    }
+}
