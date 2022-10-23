76d1e68ae441ccb03407d729de4fb65849c62b4c
yair <yairco1990@gmail.com>
2022-10-23 16:58:27 +0300

    disable suggestions on paste (length > 1) or tab(indent in)

--- src/main/java/com/tabnine/inline/CompletionUtils.kt
+++ src/main/java/com/tabnine/inline/CompletionUtils.kt
@@ -1,11 +1,24 @@
 package com.tabnine.inline
 
 import com.intellij.openapi.editor.Document
+import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
+import com.tabnine.inline.render.getTabSize
 import java.util.regex.Pattern
 
 object CompletionUtils {
     private val END_OF_LINE_VALID_PATTERN = Pattern.compile("^\\s*[)}\\]\"'`]*\\s*[:{;,]?\\s*$")
+
+    @JvmStatic
+    fun isValidDocumentChange(editor: Editor, document: Document, newOffset: Int, previousOffset: Int): Boolean {
+        val addedText = document.getText(TextRange(previousOffset, newOffset))
+        return (
+            isValidMidlinePosition(document, newOffset) &&
+                isValidNonEmptyChange(addedText.length, addedText) &&
+                isSingleCharNonWhitespaceChange(addedText) &&
+                isNotIndentationChange(addedText, editor)
+            )
+    }
 
     @JvmStatic
     fun isValidMidlinePosition(document: Document, offset: Int): Boolean {
@@ -14,5 +27,20 @@ object CompletionUtils {
         val line = document.getText(lineRange)
         val lineSuffix = line.substring(offset - lineRange.startOffset)
         return END_OF_LINE_VALID_PATTERN.matcher(lineSuffix).matches()
+    }
+
+    @JvmStatic
+    fun isValidNonEmptyChange(replacedTextLength: Int, newText: String): Boolean {
+        return replacedTextLength >= 0 && newText != ""
+    }
+
+    @JvmStatic
+    fun isSingleCharNonWhitespaceChange(newText: String): Boolean {
+        return newText.trim().length <= 1
+    }
+
+    @JvmStatic
+    fun isNotIndentationChange(newText: String, editor: Editor): Boolean {
+        return newText != getTabSize(editor)?.let { " ".repeat(it) }
     }
 }
