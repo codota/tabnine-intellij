6ded65b9a467ef658fb4828b3615d5e15082fdba
Yair Cohen <yairco1990@gmail.com>
2022-12-13 18:52:52 +0200

    DEV2-1779: fix caret listener race condition and change SuggestionShownRequest response handling (#432)
    * - fix caret listener race condition
    - change SuggestionShownRequest response handling so it will ignore the response
    
    * validate line

--- src/main/java/com/tabnine/inline/listeners/InlineCaretListener.kt
+++ src/main/java/com/tabnine/inline/listeners/InlineCaretListener.kt
@@ -11,16 +11,23 @@ import com.tabnine.inline.InlineCompletionCache
 class InlineCaretListener(private val completionPreview: CompletionPreview) : CaretListener, Disposable {
     init {
         Disposer.register(completionPreview, this)
-        ApplicationManager.getApplication().invokeLater { completionPreview.editor.caretModel.addCaretListener(this) }
+        completionPreview.editor.caretModel.addCaretListener(this)
     }
 
     override fun caretPositionChanged(event: CaretEvent) {
         if (ApplicationManager.getApplication().isUnitTestMode) {
             return
         }
+        if (isSingleOffsetChange(event)) {
+            return
+        }
 
         Disposer.dispose(completionPreview)
         InlineCompletionCache.instance.clear(event.editor)
+    }
+
+    private fun isSingleOffsetChange(event: CaretEvent): Boolean {
+        return event.oldPosition.line == event.newPosition.line && event.oldPosition.column + 1 == event.newPosition.column
     }
 
     override fun dispose() {
