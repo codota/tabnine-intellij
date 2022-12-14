b58c9c758e689dedca33838c496b42096df723fc
Yair Cohen <yairco1990@gmail.com>
2022-12-14 17:21:07 +0200

    DEV2-1779: trigger autocomplete on indentation (#434)

--- src/main/java/com/tabnine/inline/CompletionUtils.kt
+++ src/main/java/com/tabnine/inline/CompletionUtils.kt
@@ -1,24 +1,21 @@
 package com.tabnine.inline
 
 import com.intellij.openapi.editor.Document
-import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.util.TextRange
-import com.tabnine.inline.render.getTabSize
 import java.util.regex.Pattern
 
 object CompletionUtils {
     private val END_OF_LINE_VALID_PATTERN = Pattern.compile("^\\s*[)}\\]\"'`]*\\s*[:{;,]?\\s*$")
 
     @JvmStatic
-    fun isValidDocumentChange(editor: Editor, document: Document, newOffset: Int, previousOffset: Int): Boolean {
+    fun isValidDocumentChange(document: Document, newOffset: Int, previousOffset: Int): Boolean {
         if (newOffset < 0 || previousOffset > newOffset) return false
 
         val addedText = document.getText(TextRange(previousOffset, newOffset))
         return (
             isValidMidlinePosition(document, newOffset) &&
                 isValidNonEmptyChange(addedText.length, addedText) &&
-                isSingleCharNonWhitespaceChange(addedText) &&
-                isNotIndentationChange(addedText, editor)
+                isSingleCharNonWhitespaceChange(addedText)
             )
     }
 
@@ -39,10 +36,5 @@ object CompletionUtils {
     @JvmStatic
     fun isSingleCharNonWhitespaceChange(newText: String): Boolean {
         return newText.trim().length <= 1
-    }
-
-    @JvmStatic
-    fun isNotIndentationChange(newText: String, editor: Editor): Boolean {
-        return newText != getTabSize(editor)?.let { " ".repeat(it) }
     }
 }
