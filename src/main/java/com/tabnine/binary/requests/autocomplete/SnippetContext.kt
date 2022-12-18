6d964868d6ebeea10c2331ea7633f551b2271ba4
yonip23 <yoni@tabnine.com>
2022-12-18 16:49:21 +0200

    DEV2-1924 prompt additional properties

--- src/main/java/com/tabnine/binary/requests/autocomplete/SnippetContext.kt
+++ src/main/java/com/tabnine/binary/requests/autocomplete/SnippetContext.kt
@@ -9,8 +9,20 @@ data class SnippetContext(
     val is_cached: Boolean?,
     val context_len: Int?,
     val first_token_score: String?,
-    val resolved_dependencies: Boolean?,
+    val prompt_additional_properties: PromptAdditionalProperties?,
     val snippet_id: String?,
+)
+
+data class PromptAdditionalProperties(
+    val resolved_dependencies: Boolean?,
+    val max_prompt_length: Int?,
+    val prompt_enrichment_lengths: PromptEnrichmentLengths?,
+)
+
+data class PromptEnrichmentLengths(
+    val prompt_until_imports_len: Int?,
+    val prompt_after_imports_len: Int?,
+    val dependencies_len: Int?
 )
 
 data class SnippetIntentMetadata(
