f3375a55fa9e1e095986f1b721312129f3a847a7
Assaf Sapir <me@ass.af>
2022-12-20 14:59:36 +0200

    DEV2-1967: Use CNAME for tabnine-logs (#443)

--- channels/alpha/com/tabnine/config/Config.java
+++ channels/alpha/com/tabnine/config/Config.java
@@ -3,5 +3,5 @@ package com.tabnine.config;
 public class Config {
   public static final String CHANNEL = "alpha";
   public static final Boolean DISPLAY_ORIGIN = true;
-  public static final String LOGGER_HOST = "https://tabnine-logs-gateway.herokuapp.com";
+  public static final String LOGGER_HOST = "https://ide-logs-gateway.tabnine.com";
 }
--- channels/beta/com/tabnine/config/Config.java
+++ channels/beta/com/tabnine/config/Config.java
@@ -3,5 +3,5 @@ package com.tabnine.config;
 public class Config {
   public static final String CHANNEL = "beta";
   public static final Boolean DISPLAY_ORIGIN = false;
-  public static final String LOGGER_HOST = "https://tabnine-logs-gateway.herokuapp.com";
+  public static final String LOGGER_HOST = "https://ide-logs-gateway.tabnine.com";
 }
