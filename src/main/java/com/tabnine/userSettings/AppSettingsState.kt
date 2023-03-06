c0112d19179df7d5204ac8970302e81ec5b6b645
yanirs <yanir@codota.com>
2023-03-06 08:57:50 +0200

    add default url

--- src/main/java/com/tabnine/userSettings/AppSettingsState.kt
+++ src/main/java/com/tabnine/userSettings/AppSettingsState.kt
@@ -26,7 +26,7 @@ class AppSettingsState : PersistentStateComponent<AppSettingsState?> {
     var debounceTime: Long = 0
     var autoImportEnabled: Boolean = true
     var binariesFolderOverride: String = ""
-    var cloud2Url: String = ""
+    var cloud2Url: String = "https://tabnine.sec.samsung.net"
 
     private var colorState = settingsDefaultColor
 
