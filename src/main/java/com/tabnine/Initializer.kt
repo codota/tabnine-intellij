a2cc587a6fbdcbd3186a64f4c1793d0ae1f3bfb3
yonip23 <yoni@tabnine.com>
2023-04-16 19:19:10 +0300

    DEV2-2533 convert initializer to kt

---
+++ src/main/java/com/tabnine/Initializer.kt
@@ -0,0 +1,59 @@
+package com.tabnine
+
+import com.intellij.ide.plugins.PluginInstaller
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.components.ServiceManager
+import com.intellij.openapi.diagnostic.Logger
+import com.intellij.openapi.progress.ProgressIndicator
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.startup.StartupActivity
+import com.tabnine.capabilities.CapabilitiesService
+import com.tabnine.config.Config
+import com.tabnine.general.DependencyContainer
+import com.tabnine.general.StaticConfig
+import com.tabnine.lifecycle.BinaryStateService
+import com.tabnine.lifecycle.TabnineUpdater
+import com.tabnine.logging.initTabnineLogger
+import com.tabnine.notifications.ConnectionLostNotificationHandler
+import java.util.concurrent.atomic.AtomicBoolean
+
+class Initializer : PreloadingActivity(), StartupActivity {
+    private val binaryNotificationsLifecycle = DependencyContainer.instanceOfBinaryNotifications()
+    private val binaryPromotionStatusBarLifecycle = DependencyContainer.instanceOfBinaryPromotionStatusBar()
+    private val connectionLostNotificationHandler = ConnectionLostNotificationHandler()
+    private val initialized = AtomicBoolean(false)
+
+    override fun preload(indicator: ProgressIndicator) {
+        initialize()
+    }
+
+    override fun runActivity(project: Project) {
+        initialize()
+    }
+
+    private fun initialize() {
+        val shouldInitialize = !(initialized.getAndSet(true) || ApplicationManager.getApplication().isUnitTestMode)
+        if (shouldInitialize) {
+            Logger.getInstance(javaClass)
+                .info(
+                    "Initializing for " +
+                        Config.CHANNEL +
+                        " onprem=" +
+                        Config.IS_ON_PREM +
+                        ", plugin id = " +
+                        StaticConfig.TABNINE_PLUGIN_ID_RAW
+                )
+            connectionLostNotificationHandler.startConnectionLostListener()
+            ServiceManager.getService(BinaryStateService::class.java).startUpdateLoop()
+            if (!Config.IS_ON_PREM) {
+                initTabnineLogger()
+                binaryNotificationsLifecycle.poll()
+                binaryPromotionStatusBarLifecycle.poll()
+                CapabilitiesService.getInstance().init()
+                TabnineUpdater.pollUpdates()
+                PluginInstaller.addStateListener(DependencyContainer.instanceOfUninstallListener())
+            }
+        }
+    }
+}
