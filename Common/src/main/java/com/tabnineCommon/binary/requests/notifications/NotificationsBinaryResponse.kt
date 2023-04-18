43e84ea3a1005b5fb696114ee1c6ffac7495312c
Assaf Sapir <assaf@tabnine.com>
2023-04-18 17:20:20 +0300

    big

--- Common/src/main/java/com/tabnineCommon/binary/requests/notifications/NotificationsBinaryResponse.kt
+++ Common/src/main/java/com/tabnineCommon/binary/requests/notifications/NotificationsBinaryResponse.kt
@@ -2,5 +2,4 @@ package com.tabnineCommon.binary.requests.notifications
 
 import com.tabnineCommon.binary.BinaryResponse
 
-data class NotificationsBinaryResponse(var notifications: List<BinaryNotification>? = null) :
-    BinaryResponse
+data class NotificationsBinaryResponse(var notifications: List<BinaryNotification>? = null) : BinaryResponse
