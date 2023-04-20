867cec5d8259dd23459fb6aee175a08a92330a1c
Assaf Sapir <assaf@tabnine.com>
2023-04-20 11:37:44 +0300

    move around

--- TabnineSelfHosted/src/main/java/com/tabnineSelfHosted/Initializer.kt
+++ TabnineSelfHosted/src/main/java/com/tabnineSelfHosted/Initializer.kt
@@ -1,16 +1,35 @@
 package com.tabnineSelfHosted
 
+import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PreloadingActivity
+import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
+import com.tabnineCommon.lifecycle.BinaryStateService
+import com.tabnineCommon.notifications.ConnectionLostNotificationHandler
+import java.util.concurrent.atomic.AtomicBoolean
 
 class Initializer : PreloadingActivity(), StartupActivity {
     override fun preload(indicator: ProgressIndicator) {
-        SelfHostedInitializer().initialize()
+        initialize()
     }
 
     override fun runActivity(project: Project) {
+        initialize()
+    }
+
+    private fun initialize() {
+        if (initialized.getAndSet(true) || ApplicationManager.getApplication().isUnitTestMode) {
+            return
+        }
+        connectionLostNotificationHandler.startConnectionLostListener()
+        ServiceManager.getService(BinaryStateService::class.java).startUpdateLoop()
         SelfHostedInitializer().initialize()
+    }
+
+    companion object {
+        private val connectionLostNotificationHandler = ConnectionLostNotificationHandler()
+        private val initialized = AtomicBoolean(false)
     }
 }
--- TabnineSelfHostedForMarketplace/src/main/java/com/tabnineSelfHosted/SelfHostedInitializer.kt
+++ TabnineSelfHostedForMarketplace/src/main/java/com/tabnineSelfHosted/SelfHostedInitializer.kt
@@ -2,16 +2,13 @@ package com.tabnineSelfHosted
 
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.application.PreloadingActivity
-import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.startup.StartupActivity
-import com.tabnineCommon.config.Config
 import com.tabnineCommon.dialogs.Dialogs.showRestartDialog
 import com.tabnineCommon.general.StaticConfig
 import com.tabnineCommon.general.Utils
-import com.tabnineCommon.lifecycle.BinaryStateService
 import com.tabnineCommon.logging.initTabnineLogger
 import com.tabnineCommon.notifications.ConnectionLostNotificationHandler
 import com.tabnineCommon.userSettings.AppSettingsState
@@ -35,10 +32,9 @@ class SelfHostedInitializer : PreloadingActivity(), StartupActivity {
 
         Logger.getInstance(javaClass)
             .info(
-                "Initializing for ${Config.CHANNEL}, plugin id = ${StaticConfig.TABNINE_PLUGIN_ID_RAW}"
+                "Initializing for self-hosted, plugin id = ${StaticConfig.TABNINE_ENTERPRISE_ID_RAW}"
             )
-        connectionLostNotificationHandler.startConnectionLostListener()
-        ServiceManager.getService(BinaryStateService::class.java).startUpdateLoop()
+
         initTabnineLogger()
         requireSelfHostedUrl()
     }
