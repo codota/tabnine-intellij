9c617e878b1dd6de73b675982ae35be2a6cef46f
Assaf Sapir <assaf@tabnine.com>
2023-04-24 11:22:48 +0300

    DEV2-2604: Fix: Enterprise icon show as not connected, move minor things (#514)

--- Common/src/main/java/com/tabnineCommon/statusBar/StatusBarProvider.java
+++ Common/src/main/java/com/tabnineCommon/statusBar/StatusBarProvider.java
@@ -2,18 +2,15 @@ package com.tabnineCommon.statusBar;
 
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.StatusBar;
+import com.intellij.openapi.wm.StatusBarWidget;
 import com.intellij.openapi.wm.StatusBarWidgetProvider;
-import com.tabnineCommon.config.Config;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class StatusBarProvider implements StatusBarWidgetProvider {
   @Nullable
   @Override
-  public com.intellij.openapi.wm.StatusBarWidget getWidget(@NotNull Project project) {
-    if (Config.IS_SELF_HOSTED) {
-      return new TabnineEnterpriseStatusBarWidget(project);
-    }
+  public StatusBarWidget getWidget(@NotNull Project project) {
     return new TabnineStatusBarWidget(project);
   }
 
