d9c4c58d3fc3555d64529b43f7cc555231690780
yonip23 <yoni@tabnine.com>
2023-06-22 16:31:36 +0300

    more editor context

--- Tabnine/src/main/java/com/tabnine/chat/commandHandlers/GetEditorContextHandler.kt
+++ Tabnine/src/main/java/com/tabnine/chat/commandHandlers/GetEditorContextHandler.kt
@@ -4,26 +4,60 @@ import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.intellij.ide.DataManager
 import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.TextRange
+import com.intellij.psi.PsiDocumentManager
 
 data class SelectedCode(val code: String, val filePath: String)
 
-data class GetEditorContextResponsePayload(val fileCode: String, val selectedCode: String, val selectedCodeUsages: List<SelectedCode> = emptyList())
+data class GetEditorContextResponsePayload(
+    val fileCode: String = "",
+    val selectedCode: String = "",
+    val selectedCodeUsages: List<SelectedCode> = emptyList(),
+    val fileUri: String? = null,
+    val language: String? = null,
+    val lineTextAtCursor: String? = null,
+)
 
 class GetEditorContextHandler(gson: Gson) : ChatMessageHandler<Unit, GetEditorContextResponsePayload>(gson) {
     override fun handle(payload: Unit?, project: Project): GetEditorContextResponsePayload {
-        val editor = getEditorFromProject(project) ?: return GetEditorContextResponsePayload("", "")
+        val editor = getEditorFromProject(project) ?: return GetEditorContextResponsePayload()
 
         val fileCode = editor.document.text
         val selectedCode = editor.selectionModel.selectedText ?: ""
+        val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document)
+        val fileUri = psiFile?.virtualFile?.path
+        val language = psiFile?.language?.id
 
-        return GetEditorContextResponsePayload(fileCode, selectedCode)
+        val lineTextAtCursor = getLineAtCursor(editor, editor.caretModel.currentCaret.offset)
+
+        return GetEditorContextResponsePayload(
+            fileCode = fileCode,
+            selectedCode = selectedCode,
+            selectedCodeUsages = emptyList(),
+            fileUri = fileUri,
+            lineTextAtCursor = lineTextAtCursor,
+            language = language
+        )
     }
 
-    override fun deserializeRequest(data: JsonElement?) {
+    private fun getLineAtCursor(editor: Editor, offset: Int): String? {
+        return try {
+            val lineNumber = editor.document.getLineNumber(offset)
+            val lineStart = editor.document.getLineStartOffset(lineNumber)
+            val lineEnd = editor.document.getLineEndOffset(lineNumber)
+
+            editor.document.getText(TextRange(lineStart, lineEnd))
+        } catch (e: Exception) {
+            Logger.getInstance(javaClass).warn("failed to get line at cursor", e)
+            null
+        }
     }
+
+    override fun deserializeRequest(data: JsonElement?) {}
 }
 
 private fun getEditorFromProject(project: Project): Editor? {
