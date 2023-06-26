ee1abbb010bf10f99c6452adada68a3b7a07db2a
yonip23 <yoni@tabnine.com>
2023-06-26 20:02:09 +0300

    add font size

--- Tabnine/src/main/java/com/tabnine/chat/commandHandlers/InitHandler.kt
+++ Tabnine/src/main/java/com/tabnine/chat/commandHandlers/InitHandler.kt
@@ -1,6 +1,7 @@
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.intellij.openapi.editor.colors.EditorColorsManager
+import com.intellij.openapi.editor.colors.EditorFontType
 import com.intellij.openapi.project.Project
 import com.tabnine.chat.commandHandlers.ChatMessageHandler
 import java.awt.Color
@@ -9,15 +10,17 @@ import javax.swing.UIManager
 data class InitPayload(
     private val ide: String,
     private val isDarkTheme: Boolean,
-    private val colors: MutableMap<String, String>
+    private val colors: MutableMap<String, String>,
+    private val fontSize: Int,
 )
 
 class InitHandler(gson: Gson) : ChatMessageHandler<Unit, InitPayload>(gson) {
     override fun handle(payload: Unit?, project: Project): InitPayload {
         val colorPalette = readColorPalette()
         val isDarkTheme = EditorColorsManager.getInstance().isDarkEditor
+        val font = EditorColorsManager.getInstance().globalScheme.getFont(EditorFontType.PLAIN).size
 
-        return InitPayload("ij", isDarkTheme, colorPalette)
+        return InitPayload("ij", isDarkTheme, colorPalette, font)
     }
 
     private fun readColorPalette(): MutableMap<String, String> {
