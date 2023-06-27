d5e54b8bd3848fc165705ed199ecc6a248a2e93f
yonip23 <yoni@tabnine.com>
2023-06-27 16:24:49 +0300

    add isTelemetryEnabled to init request (alpha only atm)

--- Tabnine/src/main/java/com/tabnine/chat/commandHandlers/InitHandler.kt
+++ Tabnine/src/main/java/com/tabnine/chat/commandHandlers/InitHandler.kt
@@ -4,6 +4,8 @@ import com.intellij.openapi.editor.colors.EditorColorsManager
 import com.intellij.openapi.editor.colors.EditorFontType
 import com.intellij.openapi.project.Project
 import com.tabnine.chat.commandHandlers.ChatMessageHandler
+import com.tabnineCommon.capabilities.CapabilitiesService
+import com.tabnineCommon.capabilities.Capability
 import java.awt.Color
 import javax.swing.UIManager
 
@@ -12,6 +14,7 @@ data class InitPayload(
     private val isDarkTheme: Boolean,
     private val colors: MutableMap<String, String>,
     private val fontSize: Int,
+    private val isTelemetryEnabled: Boolean,
 )
 
 class InitHandler(gson: Gson) : ChatMessageHandler<Unit, InitPayload>(gson) {
@@ -20,7 +23,11 @@ class InitHandler(gson: Gson) : ChatMessageHandler<Unit, InitPayload>(gson) {
         val isDarkTheme = EditorColorsManager.getInstance().isDarkEditor
         val font = EditorColorsManager.getInstance().globalScheme.getFont(EditorFontType.PLAIN).size
 
-        return InitPayload("ij", isDarkTheme, colorPalette, font)
+        return InitPayload("ij", isDarkTheme, colorPalette, font, isTelemetryEnabled())
+    }
+
+    private fun isTelemetryEnabled(): Boolean {
+        return CapabilitiesService.getInstance().isCapabilityEnabled(Capability.ALPHA)
     }
 
     private fun readColorPalette(): MutableMap<String, String> {
