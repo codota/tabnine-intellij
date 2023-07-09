904e535b00830885c6e87a8d228f8a85ee47df17
yonip23 <yoni@tabnine.com>
2023-07-09 20:55:14 +0300

    rename

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/NonExistingCommandHandler.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/NonExistingCommandHandler.kt
@@ -6,7 +6,7 @@ import com.intellij.openapi.project.Project
 
 data class NonExistingCommandResponsePayload(private val error: String)
 
-class DefaultCommandHandler(gson: Gson, private val command: String) : ChatMessageHandler<Unit, NonExistingCommandResponsePayload>(gson) {
+class NonExistingCommandHandler(gson: Gson, private val command: String) : ChatMessageHandler<Unit, NonExistingCommandResponsePayload>(gson) {
     override fun handle(payload: Unit?, project: Project): NonExistingCommandResponsePayload {
         return NonExistingCommandResponsePayload("Command $command doesn't have a handler")
     }
