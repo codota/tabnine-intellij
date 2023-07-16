090fa8e8ba91420c656d3ceb762a68755048d7cf
Yair Cohen <yairco1990@gmail.com>
2023-07-16 08:29:25 +0300

    DEV2-3020: settings handler (#572)
    * DEV2-2899: settings handler
    
    * code review
    
    * don't show the settings icon for enterprise users
    
    * DEV2-3148: handle chat proxy (#575)
    
    * DEV2-3148: handle chat proxy
    
    * set proxy protocol
    
    * add update-chat-version job
    
    * bump chat version

--- Common/src/main/java/com/tabnineCommon/chat/TabnineChatWebViewFactory.kt
+++ Common/src/main/java/com/tabnineCommon/chat/TabnineChatWebViewFactory.kt
@@ -5,12 +5,16 @@ import com.intellij.openapi.actionSystem.ActionManager
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.tabnineCommon.binary.requests.config.StateRequest
 import com.tabnineCommon.chat.actions.TabnineActionsGroup
+import com.tabnineCommon.general.DependencyContainer
+import com.tabnineCommon.general.ServiceLevel
 import java.awt.BorderLayout
 
 class TabnineChatWebViewFactory(private val browser: ChatBrowser) : ToolWindowFactory, Disposable {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val ourGroup = TabnineActionsGroup.create(browser)
+        val stateResponse = DependencyContainer.instanceOfBinaryRequestFacade().executeRequest(StateRequest())
+        val ourGroup = TabnineActionsGroup.create(browser, stateResponse.serviceLevel == ServiceLevel.BUSINESS)
         val createActionToolbar =
             ActionManager.getInstance().createActionToolbar("Tabnine chat", ourGroup, true)
         createActionToolbar.setTargetComponent(browser.jbCefBrowser.component)
--- Common/src/main/java/com/tabnineCommon/chat/actions/TabnineActionsGroup.kt
+++ Common/src/main/java/com/tabnineCommon/chat/actions/TabnineActionsGroup.kt
@@ -1,6 +1,7 @@
 package com.tabnineCommon.chat.actions
 
 import com.intellij.openapi.actionSystem.DefaultActionGroup
+import com.tabnine.chat.actions.toolWindowActions.ChatSettingsAction
 import com.tabnineCommon.chat.ChatBrowser
 import com.tabnineCommon.chat.actions.toolWindowActions.ChatHistoryAction
 import com.tabnineCommon.chat.actions.toolWindowActions.ClearConversationAction
@@ -10,12 +11,15 @@ import com.tabnineCommon.chat.actions.toolWindowActions.SubmitFeedbackAction
 
 class TabnineActionsGroup private constructor() : DefaultActionGroup("Tabnine Chat", false) {
     companion object {
-        fun create(browser: ChatBrowser): TabnineActionsGroup {
+        fun create(browser: ChatBrowser, isEnterprise: Boolean): TabnineActionsGroup {
             val group = TabnineActionsGroup()
             group.add(ClearConversationAction(browser))
             group.add(NewConversationAction(browser))
             group.add(ChatHistoryAction(browser))
             group.add(SubmitFeedbackAction(browser))
+            if (!isEnterprise) {
+                group.add(ChatSettingsAction(browser))
+            }
             group.add(OpenDevToolsAction(browser))
 
             return group
