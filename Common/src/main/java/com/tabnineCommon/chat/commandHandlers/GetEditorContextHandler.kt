1c4d903609123b48dce587c523efee4ae93a481c
yonip23 <yoni@tabnine.com>
2023-07-19 17:38:30 +0300

    DEV2-3185 revert visible range

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/GetEditorContextHandler.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/GetEditorContextHandler.kt
@@ -6,7 +6,6 @@ import com.google.gson.JsonObject
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.diagnostic.Logger
-import com.intellij.openapi.editor.Document
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.TextRange
@@ -65,18 +64,6 @@ class GetEditorContextHandler(gson: Gson) : ChatMessageHandler<Unit, GetEditorCo
             language = language,
             metadata = metadata
         )
-    }
-
-    private fun getTextInVisibleRange(document: Document, visibleRange: TextRange?): String {
-        if (visibleRange == null) {
-            return document.text
-        }
-        return try {
-            document.getText(visibleRange)
-        } catch (e: Exception) {
-            Logger.getInstance(javaClass).warn("failed to get text in visible range", e)
-            document.text
-        }
     }
 
     private fun noEditorResponse(project: Project): GetEditorContextResponsePayload {
