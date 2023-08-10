a9e1dc34228f37cf4ccdaf3f72c8a70c33339641
yonip23 <67855609+yonip23@users.noreply.github.com>
2023-08-10 18:15:33 +0300

    DEV2-3340 run both naming conventions in parallel (#612)
    * DEV2-3340 run both naming conventions in parallel
    
    * remove unnecessary extra read action invocation

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/workspace/FindSymbolsCommandExecutor.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/workspace/FindSymbolsCommandExecutor.kt
@@ -4,8 +4,8 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.tabnineCommon.chat.commandHandlers.utils.StringCaseConverter
 import com.tabnineCommon.chat.commandHandlers.utils.SymbolsResolver
-import java.util.stream.Stream
-import kotlin.streams.toList
+import com.tabnineCommon.chat.commandHandlers.utils.submitReadAction
+import java.util.concurrent.CompletableFuture
 
 private const val MAX_RESULTS_PER_SYMBOL = 5
 
@@ -13,16 +13,25 @@ class FindSymbolsCommandExecutor : CommandsExecutor {
     override fun execute(arg: String, editor: Editor, project: Project): List<String> {
         val camelCaseArg = StringCaseConverter.toCamelCase(arg)
         val snakeCaseArg = StringCaseConverter.toSnakeCase(arg)
-        val camelCaseSymbols = SymbolsResolver.resolveSymbols(
-            project, editor.document, camelCaseArg,
-            MAX_RESULTS_PER_SYMBOL
-        )
-        val snakeCaseSymbols = SymbolsResolver.resolveSymbols(
-            project, editor.document, snakeCaseArg,
-            MAX_RESULTS_PER_SYMBOL
+
+        val tasks = listOf(
+            submitReadAction {
+                SymbolsResolver.resolveSymbols(
+                    project, editor.document, camelCaseArg,
+                    MAX_RESULTS_PER_SYMBOL
+                )
+            },
+            submitReadAction {
+                SymbolsResolver.resolveSymbols(
+                    project, editor.document, snakeCaseArg,
+                    MAX_RESULTS_PER_SYMBOL
+                )
+            }
         )
 
-        return Stream.concat(camelCaseSymbols.stream(), snakeCaseSymbols.stream())
+        CompletableFuture.allOf(*tasks.toTypedArray()).get()
+
+        return tasks.map { it.get() }.flatten()
             .map { "${it.name} - ${it.relativePath}" }
             .toList()
     }
