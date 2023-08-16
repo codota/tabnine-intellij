590778954cf63c0284dc0910de7d00b2c2b7ae12
yonip23 <yoni@tabnine.com>
2023-08-16 17:03:36 +0300

    document

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/utils/Execution.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/utils/Execution.kt
@@ -9,11 +9,21 @@ enum class ActionPermissions {
     WRITE
 }
 
+/**
+ * Runs the given action *not* now - the thread in which the action
+ * is executed depends on the permissions of the action.
+ *
+ * READ - runs on a thread provided by `AppExecutorUtil.getAppExecutorService().submit`,
+ * with read permissions - i.e. acquires a read lock.
+ * WRITE - runs on the AWT thread, with write permissions.
+ *
+ * The result of the action is then captured in a CompletableFuture, and returned.
+ */
 class AsyncAction(private val permissions: ActionPermissions) {
     fun <T> execute(action: () -> T): CompletableFuture<T> {
         val future = CompletableFuture<T>()
 
-        val completeAction: () -> Unit = {
+        val performAction: () -> Unit = {
             try {
                 val result = action()
                 future.complete(result)
@@ -25,12 +35,12 @@ class AsyncAction(private val permissions: ActionPermissions) {
         when (permissions) {
             ActionPermissions.READ -> AppExecutorUtil.getAppExecutorService().submit {
                 ApplicationManager.getApplication().runReadAction {
-                    completeAction()
+                    performAction()
                 }
             }
 
             ActionPermissions.WRITE -> ApplicationManager.getApplication().invokeLater {
-                completeAction()
+                performAction()
             }
         }
 
