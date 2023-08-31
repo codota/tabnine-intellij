7d0235156b853b4b87c0bd6fe25dedb7e9e047f2
Amir Tuval <amirtu@tabnine.com>
2023-08-31 15:08:43 +0300

    fix error UI

--- Common/src/main/java/com/tabnineCommon/chat/ChatFrame.kt
+++ Common/src/main/java/com/tabnineCommon/chat/ChatFrame.kt
@@ -7,6 +7,7 @@ import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.project.Project
+import com.intellij.ui.JBColor
 import com.tabnineCommon.binary.requests.config.StateRequest
 import com.tabnineCommon.chat.actions.TabnineActionsGroup
 import com.tabnineCommon.config.Config
@@ -14,7 +15,9 @@ import com.tabnineCommon.general.DependencyContainer
 import com.tabnineCommon.general.ServiceLevel
 import com.tabnineCommon.lifecycle.BinaryCapabilitiesChangeNotifier
 import java.awt.BorderLayout
+import java.awt.Color
 import javax.swing.BorderFactory
+import javax.swing.ImageIcon
 import javax.swing.JComponent
 import javax.swing.JEditorPane
 import javax.swing.JLabel
@@ -121,8 +124,8 @@ class ChatFrame(private val project: Project, private val messagesRouter: ChatMe
         val action = ActionManager.getInstance().getAction("ChooseRuntime")
         val imgsrc = javaClass.classLoader.getResource("images/choose-runtime-with-jcef.png")?.toString()
         val chooseRuntimePostfix = """
-            <p>This might be a result of running your IDE under a runtime that does not support the Java Chromium Embedded Framework (JCEF).</p>
-            <p>If you wish, you can <a href="https://choose-runtime">click here</a> to install a runtime that supports the JCEF browser</p>
+            <p>This issue may arise if your IDE is running on a Java runtime that does not support<br/>the Java Chromium Embedded Framework (JCEF).</p>
+            <p>If you wish, you can <a href="https://choose-runtime">click here</a> to install a JCEF-supporting runtime.</p>
             <p><img src="$imgsrc" width="600" /></p>
         """.trimIndent()
         val checkLogPostfix = """
@@ -134,8 +137,8 @@ class ChatFrame(private val project: Project, private val messagesRouter: ChatMe
         val text = JEditorPane(
             "text/html",
             """
-            <h3>Failed to open a browser</h3>
-            <p>We could not open a browser, which is required to display Tabnine chat.</p>
+            <h3 style="font-weight: normal; color: #cc3e44">Failed to open a browser</h3>
+            <p>We were not able to launch a browser, which is needed to display Tabnine chat.</p>
             $postfix
             """.trimIndent()
         ).apply {
@@ -147,10 +150,49 @@ class ChatFrame(private val project: Project, private val messagesRouter: ChatMe
                 action?.actionPerformed(AnActionEvent.createFromInputEvent(null, "TabnineChatFrame", null, DataManager.getInstance().getDataContext(text)))
             }
         }
+
+        val panel = PanelRound().apply {
+            background = JBColor(Color(0xe7, 0xe7, 0xe7), Color(0x1e, 0x1e, 0x1e))
+            roundBottomLeft = 10
+            roundBottomRight = 10
+            roundTopLeft = 10
+            roundTopRight = 10
+            layout = BorderLayout()
+            border = BorderFactory.createEmptyBorder(0, 20, 0, 20)
+        }
+
+        val innerPanel = PanelRound().apply {
+            background = JBColor(Color.WHITE, Color.BLACK)
+            roundBottomLeft = 10
+            roundBottomRight = 10
+            roundTopLeft = 10
+            roundTopRight = 10
+            layout = BorderLayout()
+            border = BorderFactory.createEmptyBorder(0, 20, 10, 20)
+        }
+
+        val tabnineIconUrl = javaClass.classLoader.getResource("icons/tabnine-icon-13px.png")
+        panel.add(
+            JLabel("Tabnine Chat", ImageIcon(tabnineIconUrl), JLabel.LEFT).apply {
+                border = BorderFactory.createEmptyBorder(10, 0, 10, 0)
+                font = font.deriveFont(16f)
+                iconTextGap = 10
+            },
+            BorderLayout.NORTH
+        )
+        panel.add(innerPanel)
+        innerPanel.add(text, BorderLayout.LINE_START)
+
+        val outerPanel = JPanel(BorderLayout()).apply {
+            border = BorderFactory.createEmptyBorder(20, 20, 10, 20)
+        }
+
+        outerPanel.add(panel)
+
         setComponents(
             listOf(
                 Pair(
-                    text, null
+                    outerPanel, BorderLayout.SOUTH
                 )
             )
         )
---
+++ Common/src/main/java/com/tabnineCommon/chat/PanelRound.java
@@ -0,0 +1,122 @@
+package com.tabnineCommon.chat;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.geom.Area;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.RoundRectangle2D;
+import javax.swing.JPanel;
+
+public class PanelRound extends JPanel {
+
+    public int getRoundTopLeft() {
+        return roundTopLeft;
+    }
+
+    public void setRoundTopLeft(int roundTopLeft) {
+        this.roundTopLeft = roundTopLeft;
+        repaint();
+    }
+
+    public int getRoundTopRight() {
+        return roundTopRight;
+    }
+
+    public void setRoundTopRight(int roundTopRight) {
+        this.roundTopRight = roundTopRight;
+        repaint();
+    }
+
+    public int getRoundBottomLeft() {
+        return roundBottomLeft;
+    }
+
+    public void setRoundBottomLeft(int roundBottomLeft) {
+        this.roundBottomLeft = roundBottomLeft;
+        repaint();
+    }
+
+    public int getRoundBottomRight() {
+        return roundBottomRight;
+    }
+
+    public void setRoundBottomRight(int roundBottomRight) {
+        this.roundBottomRight = roundBottomRight;
+        repaint();
+    }
+
+    private int roundTopLeft = 0;
+    private int roundTopRight = 0;
+    private int roundBottomLeft = 0;
+    private int roundBottomRight = 0;
+
+    public PanelRound() {
+        setOpaque(false);
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        Graphics2D g2 = (Graphics2D) grphcs.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(getBackground());
+        Area area = new Area(createRoundTopLeft());
+        if (roundTopRight > 0) {
+            area.intersect(new Area(createRoundTopRight()));
+        }
+        if (roundBottomLeft > 0) {
+            area.intersect(new Area(createRoundBottomLeft()));
+        }
+        if (roundBottomRight > 0) {
+            area.intersect(new Area(createRoundBottomRight()));
+        }
+        g2.fill(area);
+        g2.dispose();
+        super.paintComponent(grphcs);
+    }
+
+    private Shape createRoundTopLeft() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundTopLeft);
+        int roundY = Math.min(height, roundTopLeft);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundTopRight() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundTopRight);
+        int roundY = Math.min(height, roundTopRight);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, roundY / 2, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundBottomLeft() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundBottomLeft);
+        int roundY = Math.min(height, roundBottomLeft);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(roundX / 2, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+        return area;
+    }
+
+    private Shape createRoundBottomRight() {
+        int width = getWidth();
+        int height = getHeight();
+        int roundX = Math.min(width, roundBottomRight);
+        int roundY = Math.min(height, roundBottomRight);
+        Area area = new Area(new RoundRectangle2D.Double(0, 0, width, height, roundX, roundY));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width - roundX / 2, height)));
+        area.add(new Area(new Rectangle2D.Double(0, 0, width, height - roundY / 2)));
+        return area;
+    }
+}
