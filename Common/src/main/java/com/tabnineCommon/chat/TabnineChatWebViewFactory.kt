307015248d8744c37191267dd311620a4b5f1937
Amir Tuval <amirtu@tabnine.com>
2023-08-31 18:15:45 +0300

    DEV2-3512 add analytics for no browser available

--- Common/src/main/java/com/tabnineCommon/chat/TabnineChatWebViewFactory.kt
+++ Common/src/main/java/com/tabnineCommon/chat/TabnineChatWebViewFactory.kt
@@ -5,13 +5,15 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.util.Disposer
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.tabnineCommon.general.DependencyContainer.instanceOfBinaryRequestFacade
 
 class TabnineChatWebViewFactory : ToolWindowFactory, Disposable {
 
     private var messagesRouter = ChatMessagesRouter()
+    private val binaryRequestFacade = instanceOfBinaryRequestFacade()
 
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val chatFrame = ChatFrame(project, messagesRouter)
+        val chatFrame = ChatFrame(project, messagesRouter, binaryRequestFacade)
         Disposer.register(toolWindow.disposable, chatFrame)
         toolWindow.component.add(chatFrame)
     }
