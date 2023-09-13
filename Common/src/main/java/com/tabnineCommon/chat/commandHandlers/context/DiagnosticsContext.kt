4926d53c84f7448d5346e9db4b48c339897fb354
Yair Cohen <yairco1990@gmail.com>
2023-09-13 13:39:28 +0300

    DEV2-3634: add context to the selected-code and the diagnostics (#642)
    * DEV2-3634: add context to the selected-code and the diagnostics
    
    * code review

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/DiagnosticsContext.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/DiagnosticsContext.kt
@@ -1,6 +1,7 @@
 package com.tabnineCommon.chat.commandHandlers.context
 
 import com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl
+import com.intellij.codeInsight.daemon.impl.HighlightInfo
 import com.intellij.lang.annotation.HighlightSeverity
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.editor.Editor
@@ -12,8 +13,21 @@ import com.tabnineCommon.chat.commandHandlers.utils.AsyncAction
 import java.awt.Point
 import java.util.concurrent.CompletableFuture
 
+data class DiagnosticItem(val errorMessage: String, val lineNumber: Int, val lineCode: String) {
+    constructor(highlightInfo: HighlightInfo, editor: Editor) : this(
+        errorMessage = highlightInfo.description,
+        lineNumber = editor.document.getLineNumber(highlightInfo.actualStartOffset) + 1,
+        lineCode = editor.document.getText(
+            TextRange(
+                editor.document.getLineStartOffset(editor.document.getLineNumber(highlightInfo.actualStartOffset)),
+                editor.document.getLineEndOffset(editor.document.getLineNumber(highlightInfo.actualStartOffset))
+            )
+        ).trim()
+    )
+}
+
 data class DiagnosticsContext(
-    private val diagnosticsText: String? = null,
+    private val diagnostics: List<DiagnosticItem>? = null,
 ) : EnrichingContextData {
     // Used for serialization - do not remove
     private val type: EnrichingContextType = EnrichingContextType.Diagnostics
@@ -26,19 +40,20 @@ data class DiagnosticsContext(
         }
 
         private fun create(editor: Editor, project: Project): DiagnosticsContext? {
-            val visibleRange = getVisibleRange(editor) ?: return null
-            val highlights = mutableListOf<String>()
+            val rangeToCheck: TextRange = getSelectedRange(editor) ?: getVisibleRange(editor) ?: return null
+            val diagnostics = mutableListOf<DiagnosticItem>()
 
             DaemonCodeAnalyzerImpl.processHighlights(
                 editor.document,
                 project,
                 HighlightSeverity.WARNING,
-                visibleRange.startOffset,
-                visibleRange.endOffset,
-                Processor { highlights.add(it.description) }
+                rangeToCheck.startOffset,
+                rangeToCheck.endOffset,
+                Processor { highlightInfo ->
+                    diagnostics.add(DiagnosticItem(highlightInfo, editor))
+                }
             )
 
-            val diagnostics = highlights.joinToString("\n")
             Logger.getInstance(DiagnosticsContext::class.java).debug("diagnostics: $diagnostics")
 
             return DiagnosticsContext(diagnostics)
@@ -56,6 +71,15 @@ data class DiagnosticsContext(
                 TextRange(startOffset, endOffset)
             } catch (e: IllegalArgumentException) {
                 Logger.getInstance(DiagnosticsContext::class.java).warn("failed to get visible range", e)
+                null
+            }
+        }
+
+        private fun getSelectedRange(editor: Editor): TextRange? {
+            val selectionModel = editor.selectionModel
+            return if (selectionModel.hasSelection()) {
+                TextRange(selectionModel.selectionStart, selectionModel.selectionEnd)
+            } else {
                 null
             }
         }
--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/GetSelectedCodeHandler.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/GetSelectedCodeHandler.kt
@@ -2,21 +2,32 @@ package com.tabnineCommon.chat.commandHandlers.context
 
 import com.google.gson.Gson
 import com.google.gson.JsonElement
+import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
 import com.tabnineCommon.chat.commandHandlers.ChatMessageHandler
-import com.tabnineCommon.chat.commandHandlers.utils.ActionPermissions
-import com.tabnineCommon.chat.commandHandlers.utils.AsyncAction
 
-data class GetSelectedCodeResponsePayload(private val code: String)
+data class GetSelectedCodeResponsePayload(private val code: String, private val startLine: Int, private val endLine: Int)
 
 class GetSelectedCodeHandler(gson: Gson) :
     ChatMessageHandler<Unit, GetSelectedCodeResponsePayload>(gson) {
     override fun handle(payload: Unit?, project: Project): GetSelectedCodeResponsePayload? {
+        return ReadAction.compute<GetSelectedCodeResponsePayload, Throwable> { getSelectedCodeResponse(project) }
+    }
+
+    private fun getSelectedCodeResponse(project: Project): GetSelectedCodeResponsePayload? {
         val editor = getEditorFromProject(project) ?: return null
-        val selectedCode = AsyncAction(ActionPermissions.READ).execute {
-            editor.selectionModel.selectedText
-        }.join()
-        return if (selectedCode.isNullOrEmpty()) null else GetSelectedCodeResponsePayload(selectedCode)
+        val selectedCode = editor.selectionModel.selectedText
+
+        if (selectedCode.isNullOrEmpty()) return null
+
+        val selectionStartOffset = editor.selectionModel.selectionStart
+        val selectionEndOffset = editor.selectionModel.selectionEnd
+        val document = editor.document
+
+        val startLine = document.getLineNumber(selectionStartOffset) + 1
+        val endLine = document.getLineNumber(selectionEndOffset) + 1
+
+        return GetSelectedCodeResponsePayload(selectedCode, startLine, endLine)
     }
 
     override fun deserializeRequest(data: JsonElement?) {}
