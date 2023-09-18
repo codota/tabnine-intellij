1cf03573c4a29b62ed96ca9906ccbfeab9629b18
yonip23 <yoni@tabnine.com>
2023-09-18 14:24:26 +0300

    execute async

--- Common/src/main/java/com/tabnineCommon/lifecycle/TabnineFileEditorListener.kt
+++ Common/src/main/java/com/tabnineCommon/lifecycle/TabnineFileEditorListener.kt
@@ -3,6 +3,7 @@ package com.tabnineCommon.lifecycle
 import com.intellij.openapi.diagnostic.Logger
 import com.intellij.openapi.fileEditor.FileEditorManagerEvent
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.util.concurrency.AppExecutorUtil
 import com.tabnineCommon.binary.requests.fileLifecycle.PrefetchRequest
 import com.tabnineCommon.general.DependencyContainer
 
@@ -11,7 +12,12 @@ class TabnineFileEditorListener : FileEditorManagerListener {
 
     override fun selectionChanged(event: FileEditorManagerEvent) {
         super.selectionChanged(event)
-        val path = event.newEditor?.file?.path
+        AppExecutorUtil.getAppExecutorService().execute {
+            sendPrefetchRequest(event.newEditor?.file?.path)
+        }
+    }
+
+    private fun sendPrefetchRequest(path: String?) {
         if (path == null) {
             Logger.getInstance(javaClass).warn("Failed to find path for selected file, skipping prefetch request")
             return
