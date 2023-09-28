781cc0e5065754b81f0314a46f1cc488e4e58a53
Ofek Ben-Yaish <ofekbenyaish@tabnine.com>
2023-09-28 12:49:29 +0300

    Remove chat control buttons

--- Common/src/main/java/com/tabnineCommon/chat/actions/TabnineChatToolWindowAction.kt
+++ Common/src/main/java/com/tabnineCommon/chat/actions/TabnineChatToolWindowAction.kt
@@ -1,9 +1,7 @@
 package com.tabnineCommon.chat.actions
 
 import com.intellij.openapi.actionSystem.AnAction
-import com.intellij.openapi.diagnostic.Logger
 import com.tabnineCommon.chat.ChatBrowser
-import com.tabnineCommon.general.DependencyContainer
 import javax.swing.Icon
 
 data class TabnineActionRequest(val command: String, private val data: Any? = null)
@@ -14,11 +12,4 @@ abstract class TabnineChatAction(
     description: String? = text,
     icon: Icon? = null
 ) :
-    AnAction(text, description, icon) {
-    fun sendMessage(message: TabnineActionRequest) {
-        val messageJson = DependencyContainer.instanceOfGson().toJson(message)
-
-        Logger.getInstance(javaClass).info("Sending message: $messageJson")
-        browser.jbCefBrowser.cefBrowser.executeJavaScript("window.postMessage($messageJson, '*')", "", 0)
-    }
-}
+    AnAction(text, description, icon)
