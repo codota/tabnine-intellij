10a45cd57d900cfd983428ee7c4877fa2e913f28
amircodota <25413470+amircodota@users.noreply.github.com>
2023-10-11 20:21:40 +0000

    DEV2-3866 invoke binary to get server url (#656)

---
+++ Common/src/main/java/com/tabnineCommon/binary/requests/config/ChatCommunicatorAddressRequest.kt
@@ -0,0 +1,22 @@
+package com.tabnineCommon.binary.requests.config
+
+import com.google.gson.annotations.SerializedName
+import com.tabnineCommon.binary.BinaryRequest
+
+enum class ChatCommunicationKind {
+    @SerializedName("forward")
+    Forward
+}
+class ChatCommunicatorAddressRequest(val kind: ChatCommunicationKind) : BinaryRequest<ChatCommunicatorAddressResponse> {
+    override fun response(): Class<ChatCommunicatorAddressResponse> {
+        return ChatCommunicatorAddressResponse::class.java
+    }
+
+    override fun serialize(): Any {
+        return mapOf("ChatCommunicatorAddress" to this)
+    }
+
+    override fun validate(response: ChatCommunicatorAddressResponse): Boolean {
+        return true
+    }
+}
