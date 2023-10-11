05391327c54cc6ea216bea9822906d60d04393f4
amircodota <25413470+amircodota@users.noreply.github.com>
2023-10-11 20:21:40 +0000

    DEV2-3866 invoke binary to get server url (#656)

---
+++ Common/src/main/java/com/tabnineCommon/binary/requests/config/ChatCommunicatorAddressResponse.kt
@@ -0,0 +1,5 @@
+package com.tabnineCommon.binary.requests.config
+
+import com.tabnineCommon.binary.BinaryResponse
+
+data class ChatCommunicatorAddressResponse(val address: String) : BinaryResponse
--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/GetServerUrlHandler.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/GetServerUrlHandler.kt
@@ -3,16 +3,28 @@ package com.tabnineCommon.chat.commandHandlers
 import com.google.gson.Gson
 import com.google.gson.JsonElement
 import com.intellij.openapi.project.Project
+import com.tabnineCommon.binary.requests.config.ChatCommunicationKind
+import com.tabnineCommon.binary.requests.config.ChatCommunicatorAddressRequest
+import com.tabnineCommon.capabilities.CapabilitiesService
+import com.tabnineCommon.capabilities.Capability
 import com.tabnineCommon.chat.commandHandlers.utils.getServerUrl
+import com.tabnineCommon.general.DependencyContainer
 
+data class ServerUrlRequest(val kind: ChatCommunicationKind)
 data class ServerUrlResponse(val serverUrl: String)
 
-class GetServerUrlHandler(gson: Gson) : ChatMessageHandler<Unit, ServerUrlResponse>(gson) {
-    override fun handle(payload: Unit?, project: Project): ServerUrlResponse {
+class GetServerUrlHandler(gson: Gson) : ChatMessageHandler<ServerUrlRequest, ServerUrlResponse>(gson) {
+    override fun handle(payload: ServerUrlRequest?, project: Project): ServerUrlResponse {
+        if (CapabilitiesService.getInstance().isCapabilityEnabled(Capability.CHAT_URL_FROM_BINARY) && payload != null) {
+            val response = DependencyContainer.instanceOfBinaryRequestFacade().executeRequest(ChatCommunicatorAddressRequest(payload.kind))
+            return ServerUrlResponse(response.address)
+        }
+
         val serverUrl = getServerUrl() ?: "https://api.tabnine.com"
         return ServerUrlResponse(serverUrl)
     }
 
-    override fun deserializeRequest(data: JsonElement?) {
+    override fun deserializeRequest(data: JsonElement?): ServerUrlRequest {
+        return gson.fromJson(data, ServerUrlRequest::class.java)
     }
 }
