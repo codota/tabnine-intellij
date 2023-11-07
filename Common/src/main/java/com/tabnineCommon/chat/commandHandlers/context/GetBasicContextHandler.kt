0352796c470e30d128980e40b4b928a2aead1b4e
Yoni Davidson <yonidavidson@tabnine.com>
2023-11-07 12:00:18 +0200

    add code

--- Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/GetBasicContextHandler.kt
+++ Common/src/main/java/com/tabnineCommon/chat/commandHandlers/context/GetBasicContextHandler.kt
@@ -5,7 +5,10 @@ import com.google.gson.JsonElement
 import com.google.gson.JsonObject
 import com.intellij.openapi.application.ReadAction
 import com.intellij.openapi.project.Project
+import com.intellij.openapi.project.ProjectManager
 import com.intellij.psi.PsiDocumentManager
+import com.intellij.psi.PsiManager
+import com.intellij.util.indexing.FileBasedIndex
 import com.tabnineCommon.binary.requests.fileMetadata.FileMetadataRequest
 import com.tabnineCommon.chat.commandHandlers.ChatMessageHandler
 import com.tabnineCommon.general.DependencyContainer
@@ -33,7 +36,7 @@ class GetBasicContextHandler(gson: Gson) : ChatMessageHandler<Unit, BasicContext
 
         val psiFile = PsiDocumentManager.getInstance(project).getPsiFile(editor.document)
         val fileUri = psiFile?.virtualFile?.path
-        val language = psiFile?.language?.id
+        val language = psiFile?.language?.id?.let { getPredominantWorkspaceLanguage() }
 
         var metadata = if (fileUri != null) binaryRequestFacade.executeRequest(FileMetadataRequest(fileUri)) else null
 
@@ -54,7 +57,33 @@ class GetBasicContextHandler(gson: Gson) : ChatMessageHandler<Unit, BasicContext
         if (metadata?.has("error") == true) {
             metadata = null
         }
+        return BasicContext(null, getPredominantWorkspaceLanguage(), metadata)
+    }
 
-        return BasicContext(metadata)
+    private fun getPredominantWorkspaceLanguage(maxFiles: Int = 50): String? {
+        val project = ProjectManager.getInstance().openProjects.firstOrNull() ?: return null
+        val fileIndex = FileBasedIndex.getInstance()
+
+        val languageCount = mutableMapOf<String, Int>()
+
+        // Using the FileBasedIndex to iterate through files
+        fileIndex.iterateIndexableFiles(
+            { virtualFile ->
+                if (!virtualFile.isDirectory && virtualFile.isValid) {
+                    val psiFile = PsiManager.getInstance(project).findFile(virtualFile)
+                    val language = psiFile?.language?.id ?: return@iterateIndexableFiles true
+
+                    languageCount[language] = languageCount.getOrDefault(language, 0) + 1
+                }
+                true
+            },
+            project, null
+        )
+
+        // Sorting languages by frequency
+        val sortedLanguages = languageCount.toList().sortedByDescending { (_, count) -> count }
+
+        // Returning the most frequent language or null if no files are found
+        return sortedLanguages.firstOrNull()?.first
     }
 }
