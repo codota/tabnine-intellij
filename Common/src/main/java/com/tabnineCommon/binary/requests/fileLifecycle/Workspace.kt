8f745e4dad1bfc380ff6d8aef66606881afd5f95
yonip23 <yoni@tabnine.com>
2023-11-20 11:28:23 +0200

    DEV2-4253 allow null response for Workspace request

--- Common/src/main/java/com/tabnineCommon/binary/requests/fileLifecycle/Workspace.kt
+++ Common/src/main/java/com/tabnineCommon/binary/requests/fileLifecycle/Workspace.kt
@@ -2,6 +2,7 @@ package com.tabnineCommon.binary.requests.fileLifecycle
 
 import com.google.gson.annotations.SerializedName
 import com.tabnineCommon.binary.BinaryRequest
+import com.tabnineCommon.binary.exceptions.TabNineInvalidResponseException
 import com.tabnineCommon.binary.requests.EmptyResponse
 
 data class Workspace(
@@ -16,6 +17,10 @@ data class Workspace(
     }
 
     override fun validate(response: EmptyResponse): Boolean {
+        return true
+    }
+
+    override fun shouldBeAllowed(e: TabNineInvalidResponseException): Boolean {
         return true
     }
 }
