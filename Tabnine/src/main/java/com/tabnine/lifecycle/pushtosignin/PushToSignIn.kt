5550d0440a722fb6e923cd0f750b320c88f3ddac
Ofek Ben-Yaish <ofekbenyaish@tabnine.com>
2023-11-21 14:06:24 +0200

    Change to only show once

--- Tabnine/src/main/java/com/tabnine/lifecycle/pushtosignin/PushToSignIn.kt
+++ Tabnine/src/main/java/com/tabnine/lifecycle/pushtosignin/PushToSignIn.kt
@@ -12,6 +12,10 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 class PushToSignIn {
     private var started = AtomicBoolean(false)
+    private var lastIsLoggedIn: Boolean? = null
+    private var lastIsNewInstalled: Boolean? = null
+    private var lastForceRegistration: Boolean = false
+
     fun start() {
         if (started.getAndSet(true)) {
             return
@@ -39,21 +43,28 @@ class PushToSignIn {
             }
         )
     }
+
+    @Synchronized
     private fun transition(state: StateResponse? = null) {
         val loggedIn = if (state != null) state.isLoggedIn else ServiceManager.getService(BinaryStateService::class.java).lastStateResponse?.isLoggedIn
-
         val isNewInstallation = PluginInstalled.isNewInstallation
-        when {
-            !CapabilitiesService.getInstance().isCapabilityEnabled(Capability.FORCE_REGISTRATION) -> return
-            isNewInstallation == true && loggedIn == false -> {
-                presentPopup()
-                presentNotification()
-            }
-            isNewInstallation == true && loggedIn == true -> {
-                presentGreeting(state ?: ServiceManager.getService(BinaryStateService::class.java).lastStateResponse)
-            }
-            isNewInstallation == false && loggedIn == false -> {
-                presentNotification()
+        val forceRegistration = CapabilitiesService.getInstance().isCapabilityEnabled(Capability.FORCE_REGISTRATION)
+
+        if (forceRegistration && (loggedIn != lastIsLoggedIn || isNewInstallation != lastIsNewInstalled)) {
+            lastIsLoggedIn = loggedIn
+            lastIsNewInstalled = isNewInstallation
+
+            when {
+                isNewInstallation == true && loggedIn == false -> {
+                    presentPopup()
+                    presentNotification()
+                }
+                isNewInstallation == true && loggedIn == true -> {
+                    presentGreeting(state ?: ServiceManager.getService(BinaryStateService::class.java).lastStateResponse)
+                }
+                isNewInstallation == false && loggedIn == false -> {
+                    presentNotification()
+                }
             }
         }
     }
