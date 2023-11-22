66cf175e0c4a2e537ad92424c59807bef5ebe9c6
Ofek Ben-Yaish <ofekbenyaish@tabnine.com>
2023-11-22 15:20:47 +0200

    Make value volatile

--- Common/src/main/java/com/tabnineCommon/general/TopicBasedState.kt
+++ Common/src/main/java/com/tabnineCommon/general/TopicBasedState.kt
@@ -3,14 +3,18 @@ package com.tabnineCommon.general
 import com.intellij.openapi.Disposable
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.util.messages.Topic
-import java.util.Optional
+import java.util.*
 import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.locks.Lock
 import java.util.concurrent.locks.ReadWriteLock
 import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.function.Consumer
 
-open class TopicBasedState<T, S : Consumer<T>>(private val topic: Topic<S>, private var value: T? = null) {
+open class TopicBasedState<T, S : Consumer<T>>(
+    private val topic: Topic<S>,
+    @Volatile
+    private var value: T? = null
+) {
     private val rwLock: ReadWriteLock = ReentrantReadWriteLock()
     private val hasChanged = AtomicBoolean(false)
 
@@ -39,7 +43,8 @@ open class TopicBasedState<T, S : Consumer<T>>(private val topic: Topic<S>, priv
                 subscription.accept(value!!)
             }
 
-            ApplicationManager.getApplication().messageBus.connect(parent).subscribe(topic, subscription)
+            ApplicationManager.getApplication().messageBus.connect(parent)
+                .subscribe(topic, subscription)
         }
     }
 
