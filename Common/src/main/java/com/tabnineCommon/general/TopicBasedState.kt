6d8a18612c9041c18c9b7da1548d6fd476d96f64
Ofek Ben-Yaish <ofekbenyaish@tabnine.com>
2023-11-22 16:24:14 +0200

    Format

--- Common/src/main/java/com/tabnineCommon/general/TopicBasedState.kt
+++ Common/src/main/java/com/tabnineCommon/general/TopicBasedState.kt
@@ -11,9 +11,7 @@ import java.util.concurrent.locks.ReentrantReadWriteLock
 import java.util.function.Consumer
 
 open class TopicBasedState<T, S : Consumer<T>>(
-    private val topic: Topic<S>,
-    @Volatile
-    private var value: T? = null
+    private val topic: Topic<S>, @Volatile private var value: T? = null
 ) {
     private val rwLock: ReadWriteLock = ReentrantReadWriteLock()
     private val hasChanged = AtomicBoolean(false)
