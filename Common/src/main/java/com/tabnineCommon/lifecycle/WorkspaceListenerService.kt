a9eda7dbec0a377432b27f877657c6277ff192d9
Amir Bilu <amirbilu@gmail.com>
2023-11-30 14:05:02 +0200

    remove

--- Common/src/main/java/com/tabnineCommon/lifecycle/WorkspaceListenerService.kt
+++ Common/src/main/java/com/tabnineCommon/lifecycle/WorkspaceListenerService.kt
@@ -6,10 +6,7 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.roots.ProjectRootManager
 import com.intellij.util.concurrency.AppExecutorUtil
-import com.tabnineCommon.binary.requests.analytics.EventRequest
 import com.tabnineCommon.binary.requests.fileLifecycle.Workspace
-import com.tabnineCommon.capabilities.CapabilitiesService
-import com.tabnineCommon.capabilities.Capability
 import com.tabnineCommon.general.DependencyContainer
 import java.io.File
 import java.net.URL
@@ -44,21 +41,11 @@ class WorkspaceListenerService {
 
         Logger.getInstance(javaClass).info("All root paths collected: $rootPaths")
 
-        sendEvent("workspace_sending_request", mapOf("root_paths_len" to rootPaths.size.toString()))
         binaryRequestFacade.executeRequest(Workspace(rootPaths))
     }
 
-    private fun sendEvent(name: String, properties: Map<String, String>?) {
-        if (CapabilitiesService.getInstance().isCapabilityEnabled(Capability.WORKSPACE_TRACE)) {
-            binaryRequestFacade.executeRequest(EventRequest(name, properties))
-        }
-    }
-
     fun getWorkspaceRootPaths(project: Project): List<String>? {
-        sendEvent("workspace_starting", mapOf())
-
         if (project.isDisposed) {
-            sendEvent("workspace_project_disposed", mapOf())
             Logger.getInstance(javaClass).warn("Project ${project.name} is disposed, skipping root paths resolution")
             return null
         }
@@ -67,13 +54,10 @@ class WorkspaceListenerService {
         for (contentRootUrl in ProjectRootManager.getInstance(project).contentRootUrls) {
             val url = URL(contentRootUrl)
             if (url.protocol != "file") {
-                sendEvent("workspace_protocol_not_file", mapOf("path" to url.toString()))
-
                 Logger.getInstance(javaClass).debug("$url in project ${project.name} has unsupported protocol (${url.protocol})")
                 continue
             }
             if (File(url.path).list()?.isEmpty() != false) {
-                sendEvent("workspace_empty_path", mapOf("path" to url.path))
                 Logger.getInstance(javaClass).debug("${url.path} in project ${project.name} is empty, skipping")
                 continue
             }
@@ -81,7 +65,6 @@ class WorkspaceListenerService {
         }
 
         val dedupedRootPaths = dedupRootPaths(rootPaths)
-        sendEvent("workspace_after_dedup", mapOf("before_len" to rootPaths.size.toString(), "after_len" to dedupedRootPaths.size.toString()))
 
         Logger.getInstance(javaClass).debug("Root paths for project ${project.name} found: $dedupedRootPaths")
         return dedupedRootPaths
