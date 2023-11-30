b746e28b075b743881af1e803759688143b6fbcf
Dima Abramovich <60742964+dimacodota@users.noreply.github.com>
2023-11-30 10:38:17 +0200

    DEV2-4384 - get the name of the type (#695)
    * get the name of the type
    
    * fmt

--- Common/src/main/java/com/tabnineCommon/binary/ParsedBinaryIO.java
+++ Common/src/main/java/com/tabnineCommon/binary/ParsedBinaryIO.java
@@ -27,7 +27,11 @@ public class ParsedBinaryIO {
     try {
       return Optional.ofNullable(gson.fromJson(rawResponse, responseClass))
           .orElseThrow(
-              () -> new TabNineInvalidResponseException("Binary returned null as a response"));
+              () ->
+                  new TabNineInvalidResponseException(
+                      format(
+                          "Binary returned null as a response for %s",
+                          responseClass.getSimpleName())));
     } catch (TabNineInvalidResponseException | JsonSyntaxException e) {
       throw new TabNineInvalidResponseException(
           format("Binary returned illegal response: %s", rawResponse), e, rawResponse);
